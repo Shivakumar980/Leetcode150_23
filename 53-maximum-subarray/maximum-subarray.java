0f5a5981a444ce7ff97729aa33ca4e585635436e
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-06-19 17:33:12 -0400

    Time: 10 ms (6.94%) | Memory: 57.3 MB (11.63%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,17 +3,22 @@ class Solution {
         int n=nums.length;
         int max=Integer.MIN_VALUE;
         int sum=0;
+        int startIndex=-1,endIndex=-1;
+        int start=0;
         for(int i=0;i<n;i++){
+            if(sum==0) start=i;
             sum+=nums[i];
             if(sum>max){
                 max=sum;
+                startIndex=start;
+                endIndex=i;
             }
             if(sum<0){
                 sum=0;
             }
             
         }
-            
+            System.out.println(startIndex+" "+endIndex);
         return max;
     }
 }
