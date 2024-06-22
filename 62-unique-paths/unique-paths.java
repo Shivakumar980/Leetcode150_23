8dbe317fe1e58cea7ca6885fbab1c75e7983a01a
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-06-22 09:49:12 -0400

    Time: 0 ms (100.00%) | Memory: 40.5 MB (19.92%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,20 +1,14 @@
-class Solution {
-    private int uniquePathsUtil(int i,int j,int[][] dp){
-        if(i<0 || j<0) return 0;
-        if(i==0&&j==0) return 1;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+
+    class Solution {
+    public int uniquePaths(int m, int n) {
+        int N = m + n - 2;
+        int r = Math.min(m - 1, n - 1);  // Choose the smaller of the two to minim the loop
+        long res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (N - r + i) / i;
         }
-        int  up=uniquePathsUtil(i-1,j,dp);
-        int  left=uniquePathsUtil(i,j-1,dp);
-        return dp[i][j]=up+left;
+        return (int) res;
     }
 
-    public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
-        }
-        return uniquePathsUtil(m-1,n-1,dp);
-    }
+
 }
