cd7ff38888eff03a94a1fcdf9a8fb95157646935
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-06-22 12:29:40 -0400

    Time: 11 ms (41.98%) | Memory: 47.7 MB (62.16%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,29 +1,22 @@
 class Solution {
-    public List<Integer> majorityElement(int[] v) {
-       int n = v.length; //size of the array
-        List<Integer> ls = new ArrayList<>(); // list of answers
+    public List<Integer> majorityElement(int[] nums) {
+          int n=nums.length;
+        List<Integer> ans=new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            //selected element is v[i]:
-            // Checking if v[i] is not already
-            // a part of the answer:
-            if (ls.size() == 0 || ls.get(0) != v[i]) {
-                int cnt = 0;
-                for (int j = 0; j < n; j++) {
-                    // counting the frequency of v[i]
-                    if (v[j] == v[i]) {
-                        cnt++;
-                    }
-                }
-
-                // check if frquency is greater than n/3:
-                if (cnt > (n / 3))
-                    ls.add(v[i]);
-            }
-
-            if (ls.size() == 2) break;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<n;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
+        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
+            if(entry.getValue()>n/3){
+                ans.add(entry.getKey());
+            }
+        }
+        
+        return ans;
+           
+      
+    }
 
-        return ls;
-    }   
+
 }
