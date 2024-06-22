7ec34be5e504721644b121c1f51ab3778acaa65d
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-06-22 12:22:33 -0400

    Time: 3 ms (70.76%) | Memory: 48.4 MB (8.15%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,28 +1,29 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                   
-                }
-            }
-             if(count>n/3){
-                        if(ans.size()!=0 && ans.size()<=2 && ans.get(0)==nums[i]){
-                            continue;
-                        }
-                        if(ans.size()<2){
-                             ans.add(nums[i]);
-                        }
-                       
-                        
-                    }
-           
+    public List<Integer> majorityElement(int[] v) {
+       int n = v.length; //size of the array
+        List<Integer> ls = new ArrayList<>(); // list of answers
 
+        for (int i = 0; i < n; i++) {
+            //selected element is v[i]:
+            // Checking if v[i] is not already
+            // a part of the answer:
+            if (ls.size() == 0 || ls.get(0) != v[i]) {
+                int cnt = 0;
+                for (int j = 0; j < n; j++) {
+                    // counting the frequency of v[i]
+                    if (v[j] == v[i]) {
+                        cnt++;
+                    }
+                }
+
+                // check if frquency is greater than n/3:
+                if (cnt > (n / 3))
+                    ls.add(v[i]);
+            }
+
+            if (ls.size() == 2) break;
         }
-         return ans;
+
+        return ls;
     }   
 }
