b79c3be06fee381100974c06fff1e0529a7461c2
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-06-22 21:38:52 -0400

    Time: 1 ms (24.68%) | Memory: 44.6 MB (9.89%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,17 +14,23 @@
  * }
  */
 class Solution {
-    private long minVal = Long.MIN_VALUE;
+    private void isValidBSTUtil(TreeNode root,List<Integer> al){
+        if(root!=null){
+            isValidBSTUtil(root.left,al);
+            al.add(root.val);
+            isValidBSTUtil(root.right,al);
+        }
+    }
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true; 
-        if (!isValidBST(root.left)) return false;
-        
-        if (minVal >= root.val) return false; 
-
-        minVal = root.val;
-
-        if (!isValidBST(root.right)) return false;
-
+        List<Integer> al=new ArrayList<>();
+        isValidBSTUtil(root,al);
+        for(int i=1;i<al.size();i++){
+            if(al.get(i-1)<al.get(i)){
+                continue;
+            }
+            else
+                return false;
+        }
         return true;
-    } 
+    }
 }
