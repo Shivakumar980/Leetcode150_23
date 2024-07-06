9855ecd618cffb711f5657f5e117320f854dab3f
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-07-06 14:37:20 -0400

    Time: 4 ms (48.35%) | Memory: 48.9 MB (89.30%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,28 +1,38 @@
-public class Solution {
+class Solution {
 
-private int n;
-private int m;
-
-public int numIslands(char[][] grid) {
-    int count = 0;
-    n = grid.length;
-    if (n == 0) return 0;
-    m = grid[0].length;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++)
-            if (grid[i][j] == '1') {
-                DFSMarking(grid, i, j);
-                ++count;
+    private void dfs(char[][] grid,int row,int col,int[] delrow,int[] delcol,int[][] visited){
+        visited[row][col]=1;
+        grid[row][col]='0';
+        int n=grid.length;
+        int m=grid[0].length;
+        for(int i=0;i<4;i++){
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && visited[nrow][ncol]==0){
+                dfs(grid,nrow,ncol,delrow,delcol,visited);  
+               
             }
-    }    
-    return count;
+        }
+    
+        
+    }
+    public int numIslands(char[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
+        int[][] visited=new int[n][m];
+        int[] delrow=new int[]{-1,0,1,0};
+        int[] delcol=new int[]{0,1,0,-1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==0 && grid[i][j]=='1'){
+                    dfs(grid,i,j,delrow,delcol,visited);
+                    count++;
+                    
+                }
+               
+            }
+        }
+        return count;
+    }
 }
-
-private void DFSMarking(char[][] grid, int i, int j) {
-    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
-    grid[i][j] = '0';
-    DFSMarking(grid, i + 1, j);
-    DFSMarking(grid, i - 1, j);
-    DFSMarking(grid, i, j + 1);
-    DFSMarking(grid, i, j - 1);
-}}
