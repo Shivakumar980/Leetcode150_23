e2824c0c76014df0bcccb9db33196caa53756fa2
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-07-19 20:39:51 -0400

    Time: 277 ms (5.36%) | Memory: 45.5 MB (6.04%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -25,7 +25,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(rowa[i]==cola[j]){
-                    al.add(rowa[i]);
+                    al.add(matrix[i][j]);
                 }
             }
         }
