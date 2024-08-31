d184cb95224ce27b64c1b4a0136bcd5b3702a714
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-08-31 09:54:19 -0400

    Time: 3 ms (92.60%) | Memory: 43.4 MB (72.86%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st=new Stack<>();
+        HashMap<Integer,Integer> hm= new HashMap<>();
         int[] res=new int[nums1.length];
+        for(int n2:nums2){
+            while(!st.isEmpty() && n2>st.peek()){
+                hm.put(st.pop(),n2);
+            }
+            st.push(n2);
+        }
+        while(!st.isEmpty()){
+            hm.put(st.pop(),-1);
+        }
         int index=0;
         for(int n1:nums1){
-            int sum=-1;
-           
-            int target=Integer.MAX_VALUE;
-            for(int n2:nums2){
-                if(n1==n2){
-                     target=n1;
-                }
-                if(n2>target){
-                     sum=n2;
-                     break;
-                }
-            }
-            res[index++]=sum;
-
+            res[index++]=hm.get(n1);
         }
         return res;
-        
     }
 }
