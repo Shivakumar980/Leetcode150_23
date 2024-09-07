569c772522e6319d679fd8a3f10b8f74d51f869a
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-09-07 19:16:46 -0400

    Time: 2 ms (71.35%) | Memory: 42.2 MB (77.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,48 +1,55 @@
+class Pair{
+    int row;
+    int col;
+    int time;
+    public Pair(int row, int col,int time){
+        this.row=row;
+        this.col=col;
+        this.time=time;
+    }
+}
 class Solution {
-
-    private void dfs(int row,int col,int minutes, int[][] grid){
-        int n=grid.length;
-        int m=grid[0].length;
-        if(row < 0 || col < 0 || row >= n || col >= m) return;
-        if(grid[row][col] != 1 && grid[row][col] < minutes) return; 
-        grid[row][col] = minutes;
-        int delrow[]=new int[]{-1,0,1,0};
-        int delcol[]=new int[]{0,-1,0,1};
-        for(int i=0;i<4;i++){
-            int nRow=row+delrow[i];
-            int nCol=col+delcol[i];
-
-                // Continue to rot adjacent oranges recursively
-                dfs(nRow, nCol, minutes + 1, grid);
-            }
-        }
-
-    
-
-
-
     public int orangesRotting(int[][] grid) {
+        int maxTime=0;
         int n=grid.length;
         int m=grid[0].length;
-        int time=2;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    dfs(i,j,2,grid);
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-                time=Math.max(time,grid[i][j]);
-            }
-        }
-
-        return time-2;
-
+        int countFresh=0;
+        int count=0;
+        int[][] vis=new int[n][m];
+        Queue<Pair> q=new LinkedList<>();
+        int[] delrow={-1,0,1,0};
+        int[] delcol={0,-1,0,1};
         
+        for( int i=0;i<n;i++){
+            for(int j=0;j<m ;j++){
+                if(grid[i][j]==2){
+                    q.add(new Pair(i,j,0));
+                    vis[i][j]=1;
+                }
+                 else if(grid[i][j]==1){
+                    countFresh++;
+                }  
+            }
+        }
+        while(!q.isEmpty()){
+            int row=q.peek().row;
+            int col=q.peek().col;
+            int time=q.peek().time;
+            maxTime=Math.max(time,maxTime);
+            q.remove();
+           
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if(nrow>=0 && nrow< n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    q.add(new Pair(nrow,ncol,time+1));
+                    vis[nrow][ncol]=1;
+                    count++;
+                }
+            }
+
+        }
+        if(countFresh!=count) return -1;
+       return maxTime; 
     }
 }
