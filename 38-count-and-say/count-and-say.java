c281647928324f5e20c265c3f86a7d5b6f7ea99c
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-09-13 20:34:43 -0400

    Time: 131 ms (5.12%) | Memory: 44.6 MB (23.52%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,34 @@
+class Solution {
+    private String generateNextTerm(String s){
+        int count=1;
+        StringBuilder sb=new StringBuilder();
+        for(int i=1;i<s.length();i++){
+            System.out.println("hi");
+            
+            if(s.charAt(i)==s.charAt(i-1)){
+                count++;
+                System.out.println(count);
+            }
+            
+            else{
+                sb.append(count).append(s.charAt(i-1));
+                count=1;
+            }
+                
+        }
+        sb.append(count).append(s.charAt(s.length()-1));
+        return sb.toString();
+    }
+    public String countAndSay(int n) {
+        String result = "1";
+    
+        for (int i = 1; i < n; i++) {
+            result = generateNextTerm(result);
+            //System.out.println(result);
+        }
+        
+        return result;
+    }
+    
+
+}
