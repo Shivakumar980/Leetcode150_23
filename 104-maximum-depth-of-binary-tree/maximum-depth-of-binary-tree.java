eb33d177801a0f6f918f425509d05d78ebc64fab
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-09-21 15:32:50 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (63.73%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -18,6 +18,7 @@ class Solution {
         if(root==null){
             return 0;
         }
-        return 1+ Math.max(maxDepth(root.left),maxDepth(root.right));
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+        
     }
 }
