b596b55eee53972dba251139de135fb5afc706ea
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-10-03 19:58:13 -0400

    Time: 0 ms (100.00%) | Memory: 43.6 MB (43.49%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,16 +14,16 @@
  * }
  */
 class Solution {
-    public boolean isValidBST(TreeNode root) {
-        if(root==null ) return true;
-        return dfs(root,Long.MIN_VALUE,Long.MAX_VALUE);
-
-    }
-    private boolean dfs(TreeNode root, long min, long max){
+    private boolean validate(TreeNode root, long min, long max){
         if(root==null) return true;
-        if(!((min<root.val)&&(root.val<max) )){
+        if(!(min<root.val && root.val<max)){
             return false;
         }
-        return dfs(root.left,min,root.val) && dfs(root.right,root.val,max);
+        return validate(root.left,min,root.val) && validate(root.right,root.val,max);
+
+    }
+    public boolean isValidBST(TreeNode root) {
+        return validate(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        
     }
 }
