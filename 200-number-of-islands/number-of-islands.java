4a998372d927e9b5950417c0238730eed5a7921d
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-10-22 19:07:22 -0400

    Time: 27 ms (5.56%) | Memory: 49.7 MB (33.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,40 +1,30 @@
 class Solution {
-
-    private void dfs(char[][] grid,int row,int col,int[] delrow,int[] delcol,int[][] visited){
+    int[] delrow={-1,0,1,0};
+    int[] delcol={0,1,0,-1};
+    private void dfs(char[][] grid,int row,int col,int[][] visited){
         visited[row][col]=1;
-       // grid[row][col]='0';
-        int n=grid.length;
-        int m=grid[0].length;
         for(int i=0;i<4;i++){
             int nrow=row+delrow[i];
             int ncol=col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && visited[nrow][ncol]==0){
-                dfs(grid,nrow,ncol,delrow,delcol,visited);  
-               
+            if( nrow>=0 && nrow<grid.length && ncol>=0 && ncol<grid[0].length && visited[nrow][ncol]==0 && grid[nrow][ncol]=='1') {
+                dfs(grid,nrow,ncol,visited);
             }
         }
-    
-        
     }
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int count=0;
         int[][] visited=new int[n][m];
-        int[] delrow=new int[]{-1,0,1,0};
-        int[] delcol=new int[]{0,1,0,-1};
-     
+        int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(visited[i][j]==0 && grid[i][j]=='1'){
-                    
-                    dfs(grid,i,j,delrow,delcol,visited);
+                    dfs(grid,i,j,visited);
                     count++;
-                    
+                    System.out.println(i+" "+j);
                 }
-               
             }
         }
         return count;
     }
-}
+}   
