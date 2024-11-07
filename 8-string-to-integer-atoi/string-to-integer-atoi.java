8290a93657168e86d850e49a1769c7f8b73753b5
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-11-07 14:23:23 -0500

    Time: 1 ms (100.00%) | Memory: 42.1 MB (90.42%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,34 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
-
-        s=s.trim();
-        if(s.isEmpty()) return 0;
-        int sign=1;
+        int n=s.length();
         int i=0;
-        long result=0;
-        if(s.charAt(0)=='-'){
+        int sign=1;
+        while(i<n && s.charAt(i)==' '){
+            i++;
+        }
+        if(i<n &&s.charAt(i)=='+' ){
+            i++;
+        }
+        else if(i<n &&s.charAt(i)=='-'){
             sign=-1;
             i++;
         }
-        else if(s.charAt(0)=='+'){
-            i++;
-        }
-        int n=s.length();
-
+        long num=0;
         while(i<n && Character.isDigit(s.charAt(i))){
-            int digit=s.charAt(i)-'0';
-            result=(10*result)+digit;
-             if (result * sign > Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
-            } else if (result * sign < Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
+           int digit=s.charAt(i)-'0';
+             if ((num*10)+digit > Integer.MAX_VALUE ) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
+           
+             num=(num*10)+digit;
             i++;
         }
-    
-        
-
-
-        return (int)result*sign;
+        return (int)(num*sign);
     }
 }
