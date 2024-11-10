68740e40afd3fb8eae0acd030dac8e04047c98ac
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-11-10 10:26:02 -0500

    Time: 11 ms (90.22%) | Memory: 44.4 MB (69.24%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,32 +1,13 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int n=s.length();
-        int k=3;
-        return helper(s,3)-helper(s,2);
-    }
-    int helper(String s, int k){
-        HashMap<Character, Integer> hm = new HashMap<>();
-        int n = s.length();
-        int left = 0;
-        int count = 0;
-
-        for (int right = 0; right < n; right++) {
-            // Add character at `right` to the map
-            hm.put(s.charAt(right), hm.getOrDefault(s.charAt(right), 0) + 1);
-
-            // Shrink window if we exceed `k` distinct characters
-            while (hm.size() > k) {
-                char leftChar = s.charAt(left);
-                hm.put(leftChar, hm.get(leftChar) - 1);
-                if (hm.get(leftChar) == 0) {
-                    hm.remove(leftChar);
-                }
-                left++;
-            }
-
-            // Count all valid substrings ending at `right`
-            if(hm.size()<=k){
-               count += (right - left + 1);
+        int[] lastSeen=new int[3];
+        Arrays.fill(lastSeen,-1);
+        int count=0;
+        for(int i=0;i<n;i++){
+            lastSeen[s.charAt(i)-'a']=i;
+            if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
+                count+=1+Math.min(lastSeen[0],Math.min(lastSeen[1],lastSeen[2]));
             }
         }
         return count;
