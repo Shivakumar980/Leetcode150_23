72a838aaff035b4780ea3361877e03d54b7e2fbb
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-11-10 22:06:05 -0500

    Time: 2 ms (72.11%) | Memory: 42.6 MB (31.34%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,55 +1,59 @@
-class Pair{
-    int row;
-    int col;
+class Pair {
+    int first;
+    int second;
     int time;
-    public Pair(int row, int col,int time){
-        this.row=row;
-        this.col=col;
-        this.time=time;
+
+    public Pair(int first, int second, int time) {
+        this.first = first;
+        this.second = second;
+        this.time = time;
     }
 }
-class Solution {
-    public int orangesRotting(int[][] grid) {
-        int maxTime=0;
-        int n=grid.length;
-        int m=grid[0].length;
-        int countFresh=0;
-        int count=0;
-        int[][] vis=new int[n][m];
-        Queue<Pair> q=new LinkedList<>();
-        int[] delrow={-1,0,1,0};
-        int[] delcol={0,-1,0,1};
-        
-        for( int i=0;i<n;i++){
-            for(int j=0;j<m ;j++){
-                if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));
-                    vis[i][j]=1;
-                }
-                 else if(grid[i][j]==1){
-                    countFresh++;
-                }  
-            }
-        }
-        while(!q.isEmpty()){
-            int row=q.peek().row;
-            int col=q.peek().col;
-            int time=q.peek().time;
-            maxTime=Math.max(time,maxTime);
-            q.remove();
-           
-            for(int i=0;i<4;i++){
-                int nrow=row+delrow[i];
-                int ncol=col+delcol[i];
-                if(nrow>=0 && nrow< n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.add(new Pair(nrow,ncol,time+1));
-                    vis[nrow][ncol]=1;
-                    count++;
-                }
-            }
 
+class Solution {
+    int[] delrow = { 0, 1, -1, 0 };
+    int[] delcol = { 1, 0, 0, -1 };
+
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] visited = new int[n][m];
+        Queue<Pair> q = new LinkedList<>();
+        int freshOranges=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new Pair(i, j, 0));
+                    visited[i][j] = 1;
+                } else if (grid[i][j] == 1){
+                    freshOranges++;
+                }
+            }
         }
-        if(countFresh!=count) return -1;
-       return maxTime; 
+        int t = 0;
+        while (!q.isEmpty()) {
+            int row = q.peek().first;
+            int col = q.peek().second;
+            int time = q.peek().time;
+            q.poll();
+            t = Math.max(t, time);
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol] == 0
+                        && grid[nrow][ncol] == 1) {
+                    q.add(new Pair(nrow, ncol, time + 1));
+                    visited[nrow][ncol] = 1;
+                    freshOranges--;
+                }
+
+            }
+        }
+        //System.out.println(freshOranges);
+        if (freshOranges > 0)
+            return -1;
+        return t;
     }
 }
