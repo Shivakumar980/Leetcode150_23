f69d8a4386ce2557f1d5c70c87989ed78520abb5
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-11-16 12:56:52 -0500

    Time: 17 ms (50.74%) | Memory: 45.2 MB (78.51%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,67 @@
+class Pair {
+
+    int first;
+    int second;
+    int distance;
+
+    Pair(int first, int second, int distance) {
+        this.first = first;
+        this.second = second;
+        this.distance = distance;
+    }
+
+}
+
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n=grid.length;
+
+        int[] delrow={-1,0,1};
+        int[] delcol={-1,0,1};
+        
+        if(n==1 && grid[0][0]==0) return 1;
+        if(grid[0][0]==1) return -1;
+
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(0,0,1));
+        int dist[][] =new int[n][n];
+        for(int[] row: dist){
+            Arrays.fill(row,(int)1e9);
+        }
+
+
+        while(!q.isEmpty()){
+            int row=q.peek().first;
+            int col=q.peek().second;
+            int distance=q.peek().distance;
+            q.remove();
+
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    if(!(delrow[i]==0 && delcol[j]==0)){
+                         int nrow= row+delrow[i];
+                         int ncol= col + delcol[j];
+
+                        if(nrow>=0 &&nrow<n && ncol>=0 && ncol <n && grid[nrow][ncol]==0 ){
+                                if(distance+1<dist[nrow][ncol]){
+                                    dist[nrow][ncol]=1+distance;
+                                    q.add(new Pair(nrow,ncol,1+distance));
+                                }
+                              
+                        }
+
+                    }
+                   
+                }
+            }
+            
+        }
+
+        if(dist[n-1][n-1]==(int)1e9){
+            return -1;
+        }
+
+        return dist[n-1][n-1];
+
+    }
+}
