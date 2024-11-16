c5adcf632b6c584f9e013c41b5c9b384e5efc007
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-11-16 13:01:08 -0500

    Time: 16 ms (61.22%) | Memory: 45.5 MB (57.75%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -2,12 +2,12 @@ class Pair {
 
     int first;
     int second;
-    int distance;
+   // int distance;
 
-    Pair(int first, int second, int distance) {
+    Pair(int first, int second) {
         this.first = first;
         this.second = second;
-        this.distance = distance;
+       // this.distance = distance;
     }
 
 }
@@ -23,17 +23,19 @@ class Solution {
         if(grid[0][0]==1) return -1;
 
         Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(0,0,1));
+        q.add(new Pair(0,0));
         int dist[][] =new int[n][n];
+        
         for(int[] row: dist){
             Arrays.fill(row,(int)1e9);
         }
+        dist[0][0]=1;
 
 
         while(!q.isEmpty()){
             int row=q.peek().first;
             int col=q.peek().second;
-            int distance=q.peek().distance;
+            //int distance=q.peek().distance;
             q.remove();
 
             for(int i=0;i<3;i++){
@@ -43,9 +45,9 @@ class Solution {
                          int ncol= col + delcol[j];
 
                         if(nrow>=0 &&nrow<n && ncol>=0 && ncol <n && grid[nrow][ncol]==0 ){
-                                if(distance+1<dist[nrow][ncol]){
-                                    dist[nrow][ncol]=1+distance;
-                                    q.add(new Pair(nrow,ncol,1+distance));
+                                if(dist[row][col]+1<dist[nrow][ncol]){
+                                    dist[nrow][ncol]=1+dist[row][col];
+                                    q.add(new Pair(nrow,ncol));
                                 }
                               
                         }
