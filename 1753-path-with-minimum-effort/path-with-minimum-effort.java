cc749e678476ce55dbfb6786b4b9e33c3699a50c
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-11-16 15:00:02 -0500

    Time: 54 ms (41.13%) | Memory: 45 MB (69.90%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,51 @@
+class Pair{
+    int first;
+    int second;
+    int effort;
+    public Pair(int first, int second,int effort){
+        this.first=first;
+        this.second=second;
+        this.effort=effort;
+    }
+}
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int[] delrow={-1,0,1,0};
+        int[] delcol={0,-1,0,1};
+
+        int n=heights.length;
+        int m=heights[0].length;
+        int[][] effortArray=new int[n][m];
+        for(int[] row:effortArray){
+            Arrays.fill(row,(int)1e9);
+        }
+        effortArray[0][0]=0;
+
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.effort-b.effort);
+        pq.add(new Pair(0,0,0));
+
+        while(!pq.isEmpty()){
+            int row=pq.peek().first;
+            int col=pq.peek().second;
+            int effort=pq.peek().effort;
+            pq.remove();
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m ){
+
+                    int currEffort=Math.abs(heights[nrow][ncol]-heights[row][col]);
+                   int  newEffort=Math.max(currEffort,effort);
+                    if(newEffort<effortArray[nrow][ncol]){
+                        effortArray[nrow][ncol]=newEffort;
+                        pq.add( new Pair(nrow,ncol,newEffort));
+                    }
+
+                }
+            }
+        }
+
+        return effortArray[n-1][m-1];
+    }
+}
