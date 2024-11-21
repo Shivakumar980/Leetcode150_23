bafbe4ccb3fea775f198b415b6cbf67177df55fe
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-11-21 17:03:12 -0500

    Time: 0 ms (100.00%) | Memory: 40.9 MB (7.67%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,24 +1,26 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-         int[][] dp=new int[m][n];
-         for(int[] inner:dp){
-            Arrays.fill(inner,-1);
-         }
-         return uniquePathsUtil(m- 1,n-1,dp);
-    }
-    public int uniquePathsUtil(int i,int j,int[][] dp){
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int left=uniquePathsUtil(i-1,j,dp);
-        int right=uniquePathsUtil(i,j-1,dp);
-        return dp[i][j]=left+right;
 
+    private int solvePaths(int row,int col, int[][] dp,int m,int n){
+        if(row<0 || col <0 ) return 0;
+        if(row==0 && col==0) return 1;
+
+        if(dp[row][col]!=-1){
+            return dp[row][col];
+        }
+
+        int left= solvePaths(row-1,col,dp,m,n);
+        int right=solvePaths(row,col-1,dp,m,n);
+        
+        return dp[row][col]=left+right;
+
+        
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int[] row:dp)
+            Arrays.fill(row,-1);
+
+        return solvePaths(m-1,n-1,dp,m-1,n-1);
     }
 }
