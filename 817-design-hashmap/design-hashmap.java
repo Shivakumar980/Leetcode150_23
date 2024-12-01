ed7909eb392afe03ca27115ac069c18359f064fc
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-01 18:52:29 -0500

    Time: 15 ms (91.16%) | Memory: 53.1 MB (43.34%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.java
@@ -0,0 +1,81 @@
+class ListNode{
+    int key;
+    int val;
+    ListNode next;
+    public ListNode(int key,int val,ListNode next){
+        this.key=key;
+        this.val=val;
+        this.next=next;
+    }
+}
+    
+    class MyHashMap {
+
+        private static final int SIZE=1000;
+        private ListNode[] map;
+
+    public MyHashMap() {
+        map = new ListNode[SIZE];
+    }
+
+    private int hash(int key){
+        return key % SIZE ;
+    }
+    
+    public void put(int key, int value) {
+        
+        int index = hash(key);
+        if(map[index] == null){
+            map[index]=new ListNode(-1,-1,null);
+        }
+        ListNode prev= findPrevNode(map[index] ,key);
+
+        if(prev.next==null){
+            prev.next=new ListNode(key,value,null);
+        }else{
+            prev.next.val=value;
+        }
+    }
+    
+    public int get(int key) {
+        int index= hash(key);
+        if(map[index]==null){
+            return -1;
+        }
+        ListNode prev=findPrevNode(map[index],key);
+        if(prev.next==null){
+            return -1;
+        }
+        return prev.next.val;
+    }
+    
+    public void remove(int key) {
+        int index=hash(key);
+        if(map[index]==null){
+            return;
+        }
+        ListNode prev=findPrevNode(map[index],key);
+        if(prev.next!=null){
+            prev.next=prev.next.next;
+        }   
+    }
+
+    private ListNode findPrevNode(ListNode head,int key){
+        ListNode node=head;
+
+        while(node.next!=null && node.next.key!=key){
+            node=node.next;
+        }
+
+        return node;
+    }
+
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
