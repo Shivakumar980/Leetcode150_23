339f8c12a4fda797962162862755419713ae3418
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-07 20:07:36 -0500

    Time: 10 ms (51.99%) | Memory: 49.3 MB (24.84%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,30 +1,22 @@
 class Solution {
 
-    private int changeWays(int ind, int target, int[][] dp, int[] coins){
-      
-        if(ind==0){
-            if(target%coins[0]==0) return 1;
-            else return 0;
-        }
-
-        if(dp[ind][target]!=-1){
-            return dp[ind][target];
-        }
-        int not_take= 0 + changeWays(ind-1,target,dp,coins);
-        int take=0;
-        if(coins[ind]<=target){
-            take= changeWays(ind, target-coins[ind], dp, coins);
-        }
-        return dp[ind][target]=take + not_take;
-
-    }
 
     public int change(int amount, int[] coins) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int[] row: dp){
-            Arrays.fill(row,-1);
+        for(int target=0;target<=amount ;target++){
+            dp[0][target]=target % coins[0]==0 ? 1:0;
         }
-        return changeWays(n-1, amount, dp, coins);
+        for(int ind=1 ;ind <n; ind++){
+            for(int target=0; target<=amount ; target++){
+                int not_take= dp[ind-1][target];
+                int take=0;
+                if(coins[ind]<=target){
+                    take= dp[ind][target-coins[ind]];
+                }
+                dp[ind][target]= take + not_take;
+            }
+        }
+        return dp[n-1][amount];
     }
 }
