7e8b72e723887398a9eb346e2ea4a1e5afbeb4ee
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-09 18:36:45 -0500

    Time: 19 ms (59.24%) | Memory: 51.3 MB (27.93%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,35 +1,26 @@
 class Solution {
 
-    private int numDistinctUtil(int ind1, int ind2, String s, String t, int[][] dp){
-
-        
-        if(ind2<0){
-            return 1;
-        }
-        if(ind1<0){
-            return 0;
-        }
-
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-
-        if(s.charAt(ind1)==t.charAt(ind2)){
-            return dp[ind1][ind2]= numDistinctUtil(ind1-1,ind2-1,s, t, dp) +numDistinctUtil(ind1-1,ind2,s,t,dp);
-        }
-        else{
-            return dp[ind1][ind2]=numDistinctUtil(ind1-1,ind2,s,t,dp);
-        }
-
-    }
     public int numDistinct(String s, String t) {
         int n1=s.length();
         int n2=t.length();
 
-        int[][] dp=new int[n1][n2];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp=new int[n1+1][n2+1];
+
+        for(int i=0;i<=n1;i++){
+            dp[i][0]=1;
         }
-        return  numDistinctUtil(n1-1, n2-1, s, t, dp);
+
+        for(int ind1=1;ind1<=n1;ind1++){
+            for(int ind2=1; ind2<=n2 ;ind2++){
+                if(s.charAt(ind1-1)==t.charAt(ind2-1)){
+                    dp[ind1][ind2]= dp[ind1-1][ind2-1] +dp[ind1-1][ind2];
+                }
+                else{
+                    dp[ind1][ind2]= dp[ind1-1][ind2];
+                }
+            }
+        }
+        
+        return  dp[n1][n2];
     }
 }
