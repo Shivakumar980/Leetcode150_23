eec2b8fd1c63a75d4d36d7b38831f697dc9fe1cc
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-10 12:39:23 -0500

    Time: 23 ms (38.03%) | Memory: 44.3 MB (77.31%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,49 +1,41 @@
 class Solution {
 
-    private boolean isMatchUtil(int ind1, int ind2 , String s, String p, int[][] dp){
-        if(ind1<0 && ind2<0) return true;
-        if(ind1<0){
-            for(int i=0;i<=ind2;i++){
-                if(p.charAt(i)!='*'){
-                    return false;
-                }
-               
-            }
-             return true;
-        }
-        if(ind2<0 &&ind1>=0) return false;
-        
-
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2]==1 ? true:false;
-        }
-
-        if(s.charAt(ind1)==p.charAt(ind2)|| p.charAt(ind2)=='?'){
-            
-            
-             dp[ind1][ind2]=  isMatchUtil(ind1-1, ind2-1, s, p, dp)==true ? 1:0;
-        }
-        else if(p.charAt(ind2)=='*'){
-             dp[ind1][ind2]= isMatchUtil(ind1-1, ind2, s, p, dp) ||
-              isMatchUtil(ind1, ind2-1, s, p, dp) == true ? 1 : 0;
-        }
-        else{
-             dp[ind1][ind2]=0;
-        }
-        return dp[ind1][ind2]==1;
-    }
     public boolean isMatch(String s, String p) {
         int n1=s.length();
         int n2=p.length();
 
-        int[][] dp=new int[n1][n2];
-
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        boolean[][] dp=new boolean[n1+1][n2+1];
+        dp[0][0]=true;
+        boolean flag=true;
+        for(int ind2=1;ind2<=n2;ind2++){
+            if(p.charAt(ind2-1)!='*'){
+                flag= false;
+            }
+            dp[0][ind2]=flag;
+    
+        }
+        for(int ind1=1;ind1<=n1;ind1++){
+            dp[ind1][0]=false;
         }
 
-        return isMatchUtil(n1-1, n2-1, s, p, dp);
+        for(int ind1=1;ind1<=n1;ind1++){
+            for(int ind2=1;ind2<=n2;ind2++){
 
+                if(s.charAt(ind1-1)==p.charAt(ind2-1) || p.charAt(ind2-1)=='?'){
+                    dp[ind1][ind2]=dp[ind1-1][ind2-1];
+                }
+                else if(p.charAt(ind2-1)=='*'){
+                    dp[ind1][ind2]=dp[ind1-1][ind2] || dp[ind1][ind2-1];
+                }
+                else{
+                    dp[ind1][ind2]=false;
+                }
+            }
+           
+        }
+         return dp[n1][n2];
+
+        
         
     }
 }
