2a2ad01d241639d38e33dc07158938aac6fa1be2
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-10 08:30:23 -0500

    Time: 5 ms (66.82%) | Memory: 45 MB (40.89%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,33 +1,30 @@
 class Solution {
 
-    private int editUtil(int ind1, int ind2, String s1, String s2,int[][] dp){
-        if(ind1<0){
-            return ind2+1;
+
+    public int minDistance(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
+
+        int[][] dp = new int[n1+1][n2+1];
+
+        for(int ind2=0; ind2<=n2; ind2++){
+            dp[0][ind2]=ind2;
         }
-        if(ind2<0){
-            return ind1+1;
+        for(int ind1=0; ind1<=n1 ;ind1++){
+            dp[ind1][0]=ind1;
         }
 
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
+        for(int ind1=1;ind1<=n1;ind1++){
+            for(int ind2=1; ind2<=n2 ;ind2++){
+               if(s.charAt(ind1-1)==t.charAt(ind2-1)){
+                dp[ind1][ind2]=dp[ind1-1][ind2-1];
+               }
+               else{
+                dp[ind1][ind2]= Math.min(1+dp[ind1][ind2-1],Math.min(1+dp[ind1-1][ind2], 1+dp[ind1-1][ind2-1]));
+               }
             }
-        if(s1.charAt(ind1)==s2.charAt(ind2)){
-            return dp[ind1][ind2]= editUtil(ind1-1, ind2-1, s1, s2, dp);
-        }
-        else{
-            return dp[ind1][ind2]=  Math.min( 1+ editUtil(ind1-1,ind2,s1,s2,dp),Math.min( 1+editUtil(ind1,ind2-1,s1,s2,dp) , 1+editUtil(ind1-1,ind2-1,s1,s2,dp)));
-        }
-    }
-
-    public int minDistance(String word1, String word2) {
-        int n1 = word1.length();
-        int n2 = word2.length();
-
-        int[][] dp = new int[n1][n2];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
         }
 
-        return editUtil(n1 - 1, n2 - 1, word1, word2, dp);
+        return dp[n1][n2];
     }
 }
