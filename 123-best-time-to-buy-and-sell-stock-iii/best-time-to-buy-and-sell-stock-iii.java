28c31a3589a2b717f5cc795fc016defab5f1b469
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-11 18:31:56 -0500

    Time: 58 ms (46.71%) | Memory: 63.9 MB (34.59%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,31 +1,38 @@
 class Solution {
 
-    private int maxProfitUtil(int ind, int buy, int cap, int[] prices, int[][][]dp , int n){
-
-        if(cap==0 || ind==n ) return 0;
-        if(dp[ind][buy][cap]!=-1){
-            return dp[ind][buy][cap];
-        }
-        int price=0;
-        if(buy==1){
-            price = Math.max(-prices[ind]+maxProfitUtil(ind+1, 0, cap, prices, dp , n),
-            maxProfitUtil(ind+1,1,cap,prices,dp,n));
-        }
-        else{
-            price=Math.max(prices[ind]+maxProfitUtil(ind+1,1,cap-1,prices,dp,n),
-                maxProfitUtil(ind+1,0,cap,prices,dp,n));
-        }
-        return dp[ind][buy][cap]=price;
-    }
+  
     public int maxProfit(int[] prices) {
         int n=prices.length;
         int dp[][][]=new int[n+1][2][3];
-        for(int[][] twoDrow:dp){
-            for(int[] row:twoDrow){
-                Arrays.fill(row,-1);
+
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=0;cap<=2;cap++){
+                dp[n][buy][cap]=0;
             }
         }
+        for(int ind=0;ind<=n;ind++){
+            for(int buy=0;buy<=1;buy++){
+                dp[ind][buy][0]=0;
+            }
+        }
+        for(int ind=n-1; ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2; cap++){
 
-        return maxProfitUtil(0,1,2,prices,dp,n);
+                    int price=0;
+                    if(buy==1){
+                        price= Math.max(-prices[ind]+dp[ind+1][0][cap], dp[ind+1][1][cap]);
+                    }
+                    else{
+                        price= Math.max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap]);
+                    }
+
+                    dp[ind][buy][cap]=price;
+
+                }
+            }
+        }
+        
+        return dp[0][1][2];
     }
 }
