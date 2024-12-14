5b7b9e4d1aa30d1d903b967007d8261b03af4f15
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-14 14:11:48 -0500

    Time: 192 ms (15.05%) | Memory: 44.9 MB (20.32%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -5,13 +5,17 @@ class Solution {
 
         while(low<=high){
             int mid= low+ (high-low)/2;
-            if(arr.get(mid)<k){
-                low= mid+1;
-            }
-            else{
+            if(arr.get(mid)>=k){
+                //ans= arr[mid];
                 high= mid-1;
             }
+            else{
+                low= mid+1;
+            }
+            
+            
         }
+        
         return low;
     }
 
