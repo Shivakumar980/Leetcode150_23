96a58683a3e56fbaa89c6c48bd07294ad9b7e531
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-21 18:54:08 -0500

    Time: 7 ms (47.39%) | Memory: 45.6 MB (12.49%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,10 +1,8 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        
+
         Stack<Integer> st=new Stack<>();
-        int n=asteroids.length;
-        
-        for(int i=0;i<n;i++){
+        for(int i=0; i<asteroids.length ;i++){
             if(asteroids[i]>0){
                 st.push(asteroids[i]);
             }
@@ -16,23 +14,20 @@ class Solution {
                     st.pop();
                     continue;
                 }
-                else if(st.isEmpty()|| st.peek()<0){
-                    st.add(asteroids[i]);
+                else if(st.isEmpty() || st.peek()<0){
+                    st.push(asteroids[i]);
                 }
-
-
+               
             }
-           
-            
+
         }
-        int m=st.size();
-         int[] ans=new int[m];
-        
-            for(int i=m-1 ;i>=0 ;i--){
+
+            int[]  ans= new int[st.size()];
+            for(int i=st.size()-1 ;i>=0 ;i--){
                 ans[i]=st.pop();
             }
 
-            return ans;
+        return ans;
     }
-
+        
 }
