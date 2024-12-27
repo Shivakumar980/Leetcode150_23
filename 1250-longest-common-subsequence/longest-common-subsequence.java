363a3b530638152957b29461e5ca859b2897b10f
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-27 17:06:00 -0500

    Time: 19 ms (88.28%) | Memory: 45.3 MB (91.11%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,21 +5,23 @@ class Solution {
         int n1= s1.length();
         int n2= s2.length();
 
-        int dp[][]=new int[n1+1][n2+1];
+        int[] prev=new int[n2+1];
+        int[] curr=new int[n2+1];
 
        for(int ind1=1; ind1<=n1 ;ind1++){
             for(int ind2=1;ind2<=n2 ; ind2++){
                 if(s1.charAt(ind1-1)==s2.charAt(ind2-1)){
-                        dp[ind1][ind2]=1+ dp[ind1-1][ind2-1];
+                        curr[ind2]=1+ prev[ind2-1];
                     }
                 else{
-                    dp[ind1][ind2]=Math.max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                    curr[ind2]=Math.max(prev[ind2], curr[ind2-1]);
                 }
 
             
        }
+       prev= (int[]) (curr.clone());
        }
-        return dp[n1][n2];
+        return curr[n2];
         
     }
 }
