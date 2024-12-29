fa2e7709f056f157de31747c0442a36cb2c38e9e
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-29 11:29:18 -0500

    Time: 5 ms (34.31%) | Memory: 44.5 MB (53.51%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ class Pair{
+    int value;
+    ListNode node;
+    int row;
+    
+    public Pair(int value, int row,ListNode node){
+        this.value= value;
+        this.node= node;
+        this.row= row;
+    }
+}
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        int n= lists.length;
+
+        ListNode dummy =new ListNode(0);
+        ListNode head= dummy;
+        
+
+        PriorityQueue< Pair> pq= new PriorityQueue<>((a,b)-> a.value - b.value);
+
+        for(int i=0;i<n;i++){
+            ListNode node= lists[i];
+            if(node!=null) pq.add(new Pair( node.val, i, node));
+            
+        }
+        // 1,0,[1]
+        //1,1,[1]
+        //2,2,[2]
+        while(!pq.isEmpty()){
+                Pair smallest= pq.poll();
+                head.next =smallest.node;
+                head= head.next;
+
+                if(smallest.node.next!=null){
+                    pq.add(new Pair(smallest.node.next.val, smallest.row ,  smallest.node.next));
+                }
+
+        }
+
+        return dummy.next;
+        
+    }
+}
