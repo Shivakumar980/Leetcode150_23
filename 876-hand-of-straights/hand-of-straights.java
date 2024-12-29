f2edda647d0aaaecb40d0c9413063a5ff613a8f4
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2024-12-29 18:54:09 -0500

    Time: 26 ms (72.50%) | Memory: 45.4 MB (72.04%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+     
+        int n=hand.length;
+        if(n % groupSize != 0) return false;
+       
+        HashMap<Integer, Integer> hm=new HashMap<>();
+
+        for(int card: hand){
+            hm.put(card, hm.getOrDefault(card,0)+1);
+        }
+        Arrays.sort(hand);
+
+        for (int num : hand) {
+            if (hm.get(num) > 0) { // If this card is still available
+                for (int i = num; i < num + groupSize; i++) {
+                    if (hm.getOrDefault(i, 0) == 0) {
+                        return false; // Cannot form the required group
+                    }
+                    hm.put(i, hm.get(i) - 1); // Decrease the count of the current card
+                }
+            }
+        }
+        return true;
+    }
+}
+//1,2,2,3,3,4,6,7,8
