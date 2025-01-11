00c812a1964e8450ef7e757ffe3428d163521137
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2025-01-10 19:32:44 -0500

    Time: 10 ms (100.00%) | Memory: 53.4 MB (95.86%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,32 +1,24 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        
-        HashMap<Character, Integer> bmap= new HashMap<>();
-       
+        int[] bmap=new int[26];
         for(String word: words2){
-           HashMap<Character, Integer> fmap = new HashMap<>();
-                for (int i = 0; i < word.length(); i++) {
-                    char c = word.charAt(i);
-                    fmap.put(c, fmap.getOrDefault(c, 0) + 1); // Increment frequency in fmap
-                }
-                for (Map.Entry<Character, Integer> entry : fmap.entrySet()) {
-                    char c = entry.getKey();
-                    int freq = entry.getValue();
-                    bmap.put(c, Math.max(bmap.getOrDefault(c, 0), freq)); // Update bmap with max frequency
-                }
-        }
-
-       List<String> ans= new ArrayList<>();
-        for(String word: words1){
-            HashMap<Character, Integer> amap= new HashMap<>();
-            for(int i=0;i<word.length();i++){
-                char c=word.charAt(i);
-                amap.put(c, amap.getOrDefault(c,0)+1);
+            int[] fmap= new int[26];
+            for(int i=0;i<word.length(); i++){
+                char c= word.charAt(i);
+                fmap[c-'a']++;
+                bmap[c-'a']= Math.max(bmap[c-'a'], fmap[c-'a']);
             }
-             boolean flag= true;
-            for(Map.Entry<Character,Integer> entry: bmap.entrySet()){
-               
-                if(amap.get(entry.getKey())==null || amap.get(entry.getKey())<entry.getValue()){
+        }
+        List<String> ans= new ArrayList<>();
+        for(String word: words1){
+            int[] amap= new int[26];
+            for(int i=0;i<word.length(); i++){
+                char c= word.charAt(i);
+                amap[c-'a']++;
+            }
+            boolean flag=true;
+            for(int i=0;i<26;i++){
+                if(amap[i]<bmap[i]){
                     flag=false;
                     break;
                 }
