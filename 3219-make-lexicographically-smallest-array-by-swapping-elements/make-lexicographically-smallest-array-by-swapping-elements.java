60aa37c387f2d70d2cb5cfae02c32f887a01e35b
Shivakumar980 <63966962+Shivakumar980@users.noreply.github.com>
2025-01-25 17:39:28 -0500

    Time: 79 ms (81.25%) | Memory: 73.6 MB (12.50%) - LeetSync

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+
+        int[] sortedArray = nums.clone();
+        Arrays.sort(sortedArray);
+
+        int n=nums.length;
+        
+        List<Queue<Integer>> qlist=new ArrayList<>();
+        HashMap<Integer , Integer> hm= new HashMap<>();
+
+        int groupNumber=0;
+        Queue<Integer> q1= new LinkedList<>();
+        q1.add(sortedArray[0]);
+        qlist.add(q1);
+        hm.put(sortedArray[0],groupNumber);
+        for(int i=1; i<n ;i++){
+            if(Math.abs(sortedArray[i]-sortedArray[i-1])<=limit){
+                qlist.get(groupNumber).add(sortedArray[i]);
+                hm.put(sortedArray[i],groupNumber);
+            }
+            else{
+                Queue<Integer> q= new LinkedList<>();
+                q.add(sortedArray[i]);
+                qlist.add(q);
+                groupNumber++;
+                hm.put(sortedArray[i],groupNumber );
+            }
+        }
+
+        for(int i=0; i<n ;i++){
+           if (hm.containsKey(nums[i])) {
+                int index = hm.get(nums[i]);
+                nums[i] = qlist.get(index).poll(); // Poll from the corresponding queue
+            } else {
+                // If key doesn't exist in hm, handle it (though this case shouldn't happen in current logic)
+            }
+            
+        }
+
+        return nums;
+
+    }
+}
