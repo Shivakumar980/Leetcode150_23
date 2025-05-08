fc242c91ce4bc01e1a70598d1efd79c671ac1ba6
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-08 13:11:48 -0400

    Time: 16 ms (43.42%) | Memory: 45.8 MB (52.01%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,20 +1,27 @@
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> hm=new HashMap<>();
-        int n=s.length();
-        for(char c: s.toCharArray()){
-           hm.put(c,hm.getOrDefault(c,0)+1);
-        }
-        PriorityQueue<Map.Entry<Character,Integer>> pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
-        pq.addAll(hm.entrySet());
+        HashMap<Character, Integer> hm= new HashMap<Character, Integer>();
 
-        StringBuilder sb=new StringBuilder();
+        int n= s.length();
+
+        for(int i=0; i< n ;i++){
+            char c= s.charAt(i);
+            hm.put(c, hm.getOrDefault(c,0)+1);
+        }
+
+        PriorityQueue<Map.Entry<Character, Integer>> pq= new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+
+        for(Map.Entry<Character, Integer> entry:hm.entrySet()){
+            pq.add(entry);
+        }
+
+        StringBuilder sb= new StringBuilder();
         while(!pq.isEmpty()){
-            Map.Entry<Character, Integer> entry=pq.poll();
-            char c=entry.getKey();
-            int frequency=entry.getValue();
-            for(int i=0;i<frequency;i++){
-                sb.append(c);
+            Map.Entry<Character, Integer> entry= pq.poll();
+            int count= entry.getValue();
+            while(count>0){
+                sb.append(entry.getKey());
+                count--;
             }
         }
 
