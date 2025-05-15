e246eacc9f0f8faacd7c64e36b1530ddc64724e6
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-14 21:41:12 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (38.19%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,28 +1,37 @@
-
 class MyQueue {
+
     Stack<Integer> st1;
     Stack<Integer> st2;
+
     public MyQueue() {
-        st1=new Stack<>();
-        st2=new Stack<>();
+        st1= new Stack<>();
+        st2= new Stack<>();
     }
     
     public void push(int x) {
-        while(!st1.isEmpty()){
-            st2.push(st1.pop());
-        }
         st1.push(x);
-        while(!st2.isEmpty()){
-            st1.push(st2.pop());
-        }
     }
     
     public int pop() {
-        return st1.pop();
+        while(!st1.isEmpty()){
+            st2.push(st1.pop());
+        }
+        int x= st2.pop();
+        while(!st2.isEmpty()){
+            st1.push(st2.pop());
+        }
+        return x;
     }
     
     public int peek() {
-        return st1.peek();
+        while(!st1.isEmpty()){
+            st2.push(st1.pop());
+        }
+        int x= st2.peek();
+        while(!st2.isEmpty()){
+            st1.push(st2.pop());
+        }
+        return x;
     }
     
     public boolean empty() {
