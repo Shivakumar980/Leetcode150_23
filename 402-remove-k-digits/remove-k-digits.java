bb9ea652e0f1446742c2ebd126f2d60d347bc6c1
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-15 20:18:02 -0400

    Time: 27 ms (32.76%) | Memory: 45.7 MB (54.82%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,30 +1,34 @@
 class Solution {
-    public String removeKdigits(String s, int k) {
-        int n = s.length();
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i) ;
-            while (!st.isEmpty() && k > 0 && st.peek() > c) {
+    public String removeKdigits(String num, int k) {
+        int n= num.length();
+        Stack<Character> st= new Stack<>();
+
+        for(int i=0; i< n; i++){
+            char c= num.charAt(i);
+            while(!st.isEmpty() && k>0 && st.peek()> c){
                 st.pop();
                 k--;
             }
             st.push(c);
         }
-
-        while (k > 0) {
+        //edge case , more numbers added 123456, , remove last 3
+        while(k>0){
             st.pop();
             k--;
         }
-        
-        StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty()) {
+
+        StringBuilder sb= new StringBuilder();
+
+        while(!st.isEmpty()){
             sb.append(st.pop());
         }
         sb.reverse();
-        while (sb.length() > 0 && sb.charAt(0) == '0') {
+        // edge case to remove zeroes at the beginning
+        while(sb.length()>0 && sb.charAt(0)=='0'){
             sb.deleteCharAt(0);
         }
 
-        return sb.length()==0 ? "0" : sb.toString();
+        return sb.length()==0 ? "0": sb.toString();
+
     }
 }
