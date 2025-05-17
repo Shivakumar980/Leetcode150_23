9de3abad16c9a026c1491cc9d7cdb14347c6028c
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-16 21:19:24 -0400

    Time: 13 ms (76.68%) | Memory: 49.2 MB (18.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,30 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n= nums.length;
-        HashMap<Integer, Integer> hm= new HashMap<>();
-
-        for(int num:nums){
+        
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for( int num: nums){
             hm.put(num, hm.getOrDefault(num,0)+1);
         }
-        ArrayList<Integer>[] bucket= new ArrayList[nums.length+1];
 
-        for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
-            if(bucket[entry.getValue()]==null){
-                bucket[entry.getValue()]=new ArrayList<>();
-            }
-            bucket[entry.getValue()].add(entry.getKey());
-        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq= new PriorityQueue<>((a,b)-> a.getValue()-b.getValue());
 
-        int[] ans= new int[k];
-        for(int i=n; i>=0; i--){
-            if(bucket[i]!=null){
-                for(int element: bucket[i]){
-                    ans[--k]=element;
-                }
-                if(k==0) return ans;
+        for(Map.Entry<Integer, Integer> entry: hm.entrySet()){
+            pq.add(entry);
+            if(pq.size()>k){
+                pq.poll();
             }
         }
-        return ans;
+
+        int[] res= new int[pq.size()];
+        int index= pq.size()-1;
+        while(!pq.isEmpty()){
+            res[index--]=pq.poll().getKey();
+        }
+
+        return res;
+
     }
 }
-
