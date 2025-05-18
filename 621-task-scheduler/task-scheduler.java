9cf0f01e5310e3f746242ece813a6d737d63970f
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-17 20:24:36 -0400

    Time: 24 ms (45.14%) | Memory: 45.9 MB (54.91%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,48 +1,52 @@
-//ab_ab_ab
-//ababcd
-//ab__ab__ab
-class Pair {
-        int value;
-        int tm;
-
-        Pair(int value, int tm) {
-            this.value = value;
-            this.tm = tm;
-        }
+class Pair{
+    int value;
+    int time;
+    public Pair(int value, int time){
+        this.value=value;
+        this.time=time;
     }
+}
+    
 class Solution {
     public int leastInterval(char[] tasks, int n) {
 
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
-        int[] freq=new int[26];
-        for(int task : tasks){
-            freq[task-'A']++;
+        int[] fmap= new int[26];
+
+        for(char task: tasks){
+            fmap[task-'A']++;
         }
 
-        for(int val: freq){
-            if(val>0){
-                pq.add(val);
-            }
-        }
-        Queue<Pair> q=new LinkedList<>();
 
-        int time=0;
+
+        PriorityQueue<Integer> pq= new PriorityQueue<>((a,b)->b-a);
+        for(int val: fmap){
+            if(val>0) {
+                pq.offer(val);
+            };
+        }
+
+        Queue<Pair> q= new LinkedList<>();
+        int tm=0;
         while(!pq.isEmpty() || !q.isEmpty()){
-            time++;
+
             if(!pq.isEmpty()){
-                  int smallest=pq.poll();
-                  smallest--;
-                  if(smallest>0){
-                    q.add(new Pair(smallest, time+n));
-                  }
+                int val= pq.poll();
+                val-=1;
+                if(val>0){
+                    q.add(new Pair(val,tm+n));
+                }
+            } 
+
+            if(!q.isEmpty() &&q.peek().time==tm){
+                Pair p= q.poll();
+                pq.add(p.value);
             }
-     
-            if(!q.isEmpty() && q.peek().tm==time){
-                int fval=q.poll().value;
-                pq.add(fval);
-            }
+              
+            tm++;
 
         }
-        return time;
+
+        return tm;
+        
     }
 }
