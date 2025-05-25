cf972f9bbdd489c28c2151d40e643ba9caeeb547
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-25 13:53:28 -0400

    Time: 1 ms (99.87%) | Memory: 45.8 MB (56.42%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int n= nums.length;
+        int last_index=n-1;
+
+        for(int i=n-2; i>=0 ;i--){
+
+                if(nums[i]>=(last_index)-i){
+                    last_index=i;
+                }
+        }
+
+        return last_index==0;
+        
+    }
+}
