b0f6f679a74e140ecdc5c87bb6fd75fd7a784ea8
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-25 16:47:48 -0400

    Time: 33 ms (68.38%) | Memory: 46.3 MB (10.04%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,27 +1,34 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-     
-        int n=hand.length;
-        if(n % groupSize != 0) return false;
-       
-        HashMap<Integer, Integer> hm=new HashMap<>();
+        int n= hand.length;
 
-        for(int card: hand){
-            hm.put(card, hm.getOrDefault(card,0)+1);
+        if(n % groupSize!=0) return false;
+
+        HashMap<Integer, Integer> count= new HashMap<>();
+
+        for(int num : hand){
+            count.put(num, count.getOrDefault(num,0)+1);
         }
-        Arrays.sort(hand);
 
-        for (int num : hand) {
-            if (hm.get(num) > 0) { // If this card is still available
-                for (int i = num; i < num + groupSize; i++) {
-                    if (hm.getOrDefault(i, 0) == 0) {
-                        return false; // Cannot form the required group
-                    }
-                    hm.put(i, hm.get(i) - 1); // Decrease the count of the current card
+        PriorityQueue<Integer> pq= new PriorityQueue<>(count.keySet());
+
+        while(!pq.isEmpty()){
+            int key= pq.peek();
+
+            for(int i=key; i<key+ groupSize ;i++){
+                if(!count.containsKey(i)){
+                    return false;
                 }
+                count.put(i, count.get(i)-1);
+                if(count.get(i)==0){
+                    if(pq.peek()!=i){
+                        return false;
+                    }
+                    pq.poll();
+                }
+                
             }
         }
         return true;
     }
 }
-//1,2,2,3,3,4,6,7,8
