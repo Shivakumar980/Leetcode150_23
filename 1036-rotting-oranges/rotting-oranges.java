70055f6004f8f1506a0bcc714f27e1fa402d59a9
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-05-28 11:53:04 -0400

    Time: 2 ms (68.26%) | Memory: 42.7 MB (30.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,59 +1,64 @@
-class Pair {
+class Tuple{
     int first;
     int second;
     int time;
 
-    public Pair(int first, int second, int time) {
-        this.first = first;
-        this.second = second;
-        this.time = time;
+    public Tuple(int first, int second, int time){
+        this.first=first;
+        this.second=second;
+        this.time= time;
     }
 }
-
 class Solution {
-    int[] delrow = { 0, 1, -1, 0 };
-    int[] delcol = { 1, 0, 0, -1 };
-
+        int[] delrow={0,1,0,-1};
+        int[] delcol={1,0,-1,0};
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        int n= grid.length;
+        int m= grid[0].length;
 
-        int[][] visited = new int[n][m];
-        Queue<Pair> q = new LinkedList<>();
+        Queue<Tuple> q= new LinkedList<>();
+        int[][] visited= new int[n][m];
         int freshOranges=0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.add(new Pair(i, j, 0));
-                    visited[i][j] = 1;
-                } else if (grid[i][j] == 1){
+        for(int i=0; i<n ;i++){
+            for(int j=0 ; j<m ; j++){
+                if(grid[i][j]==2){
+                    q.add(new Tuple(i, j, 0));
+                    visited[i][j]=1;
+                }
+                else if(grid[i][j]==1){
                     freshOranges++;
                 }
             }
         }
-        int t = 0;
-        while (!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            int time = q.peek().time;
+
+        int t=0;
+        while(!q.isEmpty()){
+            int row=q.peek().first;
+            int col= q.peek().second;
+            int tm= q.peek().time;
             q.poll();
-            t = Math.max(t, time);
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol] == 0
-                        && grid[nrow][ncol] == 1) {
-                    q.add(new Pair(nrow, ncol, time + 1));
-                    visited[nrow][ncol] = 1;
+
+            t=Math.max(t,tm);
+
+            for(int i=0 ; i< 4; i++){
+                int nrow= row+delrow[i];
+                int ncol= col+delcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol< m && visited[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                      q.add(new Tuple(nrow, ncol, tm+1 ));
+
+                    visited[nrow][ncol]=1;
                     freshOranges--;
                 }
-
+              
             }
         }
-        //System.out.println(freshOranges);
-        if (freshOranges > 0)
+        if(freshOranges>0 ){
             return -1;
+        }
+
         return t;
+
+
     }
 }
