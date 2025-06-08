bb4f9a46af2d92a376edfe85be1e658b72d1268b
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-08 13:31:09 -0400

    Time: 5 ms (80.21%) | Memory: 45.3 MB (70.23%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,42 +1,41 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
-        int n=prerequisites.length;
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        List<List<Integer>> adj= new ArrayList<>();
+
+         for(int i=0; i< numCourses; i++){
             adj.add(new ArrayList<>());
         }
         for(int[] edge:prerequisites){
             adj.get(edge[1]).add(edge[0]);
         }
+
         int[] indegree= new int[numCourses];
 
-        Queue<Integer> q=new LinkedList<>();
-
-        for(int i=0;i<numCourses;i++){
-            for(int element:adj.get(i)){
-                indegree[element]++;
-            }
-        } 
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
-        } 
-        int count=0;
-        while(!q.isEmpty()){
-            int node=q.peek();
-            q.remove();
-            count++;
-            for(int element:adj.get(node)){
-                indegree[element]--;
-                if(indegree[element]==0){
-                    q.add(element);
-                }
-            }
+        for(int[] edge:prerequisites){
+            indegree[edge[0]]++;
         }
 
-       return count==numCourses;
+        Queue<Integer> q= new LinkedList<Integer>();
 
+        for(int i=0; i< numCourses; i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        int count=0;
+        while(!q.isEmpty()){
+            int node= q.poll();
+            count++;
+            for(int neighbour: adj.get(node)){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.offer(neighbour);
+                }
+            }
+
+        }
+        return count==numCourses;
+        
     }
 }
