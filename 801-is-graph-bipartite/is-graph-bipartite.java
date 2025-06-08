a243ef5b55fc00c7f7498d446c4af1561c521e3e
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-08 12:51:26 -0400

    Time: 0 ms (100.00%) | Memory: 45.2 MB (52.93%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,38 +1,34 @@
 class Solution {
-    int[] color;
     public boolean isBipartite(int[][] graph) {
-        int n=graph.length;
-        //int m=graph[0].length;
-        color=new int[n];
-        Arrays.fill(color,-1);
-    
-        for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                if(dfs(i,1,graph)==false){
-                    return false;
+        int n= graph.length;
+
+        int[] colorArray= new int[n];
+        Arrays.fill(colorArray,-1);
+
+        for(int i=0; i< n ;i++){
+            if(colorArray[i] == -1){
+                if(!dfs(i,1, graph,colorArray)){
+                     return false;
                 }
             }
            
         }
         return true;
     }
-    private boolean dfs(int node, int parentColor,int[][] graph){
 
-            color[node]=1-parentColor;
-    
-            
-            for(int element:graph[node]){
-                if(color[element]!=-1 && color[element]==color[node]){
+    private boolean dfs(int node, int parentcolor, int[][] graph,int[] colorArray){
+        colorArray[node]=parentcolor;
+        for(int neighbor: graph[node]){
+            if(colorArray[neighbor]==-1){
+                if(!dfs(neighbor,1-colorArray[node],graph , colorArray)){
                     return false;
                 }
-                else if(color[element]==-1 ){
-                     if(dfs(element,color[node],graph)==false){
-                        return false;
-                     }
-                }  
             }
-
-            return true;
+            else if(colorArray[neighbor]==parentcolor){
+                return false;
+            }
+        }
+        return true;
 
     }
 }
