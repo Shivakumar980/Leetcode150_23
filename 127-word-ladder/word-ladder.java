7bcdc0264863a48bfdbe9a7ca41089485994ddc8
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-08 17:10:15 -0400

    Time: 71 ms (76.19%) | Memory: 47.1 MB (41.60%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -3,50 +3,45 @@ class Pair{
     int steps;
 
     public Pair(String word, int steps){
-        this.word=word;
+        this.word= word;
         this.steps=steps;
     }
 }
-
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-
-        int n = wordList.size();
-
-        HashSet<String> hs= new HashSet<>();
-
-        for (int i=0 ; i<n ;i++){
-            hs.add(wordList.get(i));
+        
+        HashSet<String> wordSet= new HashSet<>();
+        for(String s: wordList){
+            wordSet.add(s);
         }
 
-        Queue<Pair> q=new LinkedList<>();
-
-        q.add(new Pair(beginWord,1));
+        Queue<Pair> q= new LinkedList<>();
+        q.offer(new Pair(beginWord,1));
 
         while(!q.isEmpty()){
-
-            String word=q.peek().word;
-            int steps=q.peek().steps;
+            String word= q.peek().word;
+            int steps= q.peek().steps;
             q.remove();
-
             if(word.equals(endWord)) return steps;
 
-            for(int i=0; i < word.length() ;i++){
-                char[] replacedCharArray = word.toCharArray();
-                for(char ch='a'; ch<='z';ch++){
-                    if(replacedCharArray[i]==ch){
+            for(int i=0 ; i< word.length(); i++){
+                char[] tempArray= word.toCharArray();
+
+                for(char c='a'; c<='z'; c++){
+                   
+                    if(tempArray[i]==c){
                         continue;
                     }
-                    replacedCharArray[i]=ch;
-                    String replacedWord=new String(replacedCharArray);
-                    if(hs.contains(replacedWord)){
-                        hs.remove(replacedWord);
-                        q.add(new Pair(replacedWord,steps+1));
+                    tempArray[i]=c;
+                    String tempword= new String(tempArray);
+                    if(wordSet.contains(tempword)){
+                        wordSet.remove(tempword);
+                        q.offer(new Pair(tempword,steps+1));
                     }
                 }
             }
-        }
 
+        }
         return 0;
     }
 }
