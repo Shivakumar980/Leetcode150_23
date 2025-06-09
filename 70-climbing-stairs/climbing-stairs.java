5beffd5528258b8bb0338d0e434f3b323b0c9b75
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-09 19:21:15 -0400

    Time: 0 ms (100.00%) | Memory: 40.6 MB (28.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,23 +1,22 @@
 class Solution {
-
-    private int climbStairsUtil(int ind, int[] dp){
-        if(ind==0 || ind ==1) return 1;
-
-
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-
-        int onestep= climbStairsUtil(ind-1,dp);
-        int twostep=0;
-        if(ind>1){
-            twostep= climbStairsUtil(ind-2,dp);
-        }
-        return dp[ind]=onestep +twostep;
-    }
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
+
+        int[] dp= new int[n+1];
         Arrays.fill(dp,-1);
-        return climbStairsUtil(n , dp);
+
+        return climb(dp, n);
+    }
+
+    private int climb(int[] dp, int n){
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int one= climb(dp,n-1);
+        int two=0;
+        if(n>1){
+            two=climb(dp,n-2);
+        }
+        return dp[n]=one +two;
     }
 }
