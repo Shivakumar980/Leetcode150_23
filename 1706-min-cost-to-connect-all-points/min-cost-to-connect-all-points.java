9686e8e2ea5dfe65721b4a41c79ebaa8aab8b46c
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-09 18:06:15 -0400

    Time: 557 ms (25.34%) | Memory: 94.6 MB (10.13%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,40 +1,54 @@
-public class Solution {
+class Pair{
+    int node;
+    int weight;
+    public Pair(int node, int weight){
+        this.node=node;
+        this.weight=weight;
+    }
+}
+
+class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int N = points.length;
-        Map<Integer, List<int[]>> adj = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            int x1 = points[i][0];
-            int y1 = points[i][1];
-            for (int j = i + 1; j < N; j++) {
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-                int dist = Math.abs(x1 - x2) + Math.abs(y1 - y2);
-                adj.computeIfAbsent(i, k -> new ArrayList<>()).add(new int[]{dist, j});
-                adj.computeIfAbsent(j, k -> new ArrayList<>()).add(new int[]{dist, i});
+
+        List<List<Pair>> adj= new ArrayList<>();
+        int n= points.length;
+
+        for(int i=0 ;i< n ;i++){
+            adj.add(new ArrayList<Pair>());
+        }
+
+        for(int i=0; i< n ;i++){
+            for(int j=i+1; j< n ;j++){
+                int distance= Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
+                adj.get(i).add(new Pair(j,distance));
+                adj.get(j).add(new Pair(i,distance));
             }
         }
 
-        int res = 0;
-        Set<Integer> visit = new HashSet<>();
-        PriorityQueue<int[]> minH = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        minH.offer(new int[]{0, 0});
-        while (visit.size() < N) {
-            int[] curr = minH.poll();
-            int cost = curr[0];
-            int i = curr[1];
-            if (visit.contains(i)) {
+        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)-> a.weight-b.weight);
+        int[] visited= new int[n];
+        pq.add(new Pair(0,0));
+       
+
+        int total_cost=0;
+        while(!pq.isEmpty()){
+            int nd= pq.peek().node;
+            int wt= pq.peek().weight;
+            pq.poll();
+            if(visited[nd]==1){
                 continue;
             }
-            res += cost;
-            visit.add(i);
-            for (int[] nei : adj.getOrDefault(i, Collections.emptyList())) {
-                int neiCost = nei[0];
-                int neiIndex = nei[1];
-                if (!visit.contains(neiIndex)) {
-                    minH.offer(new int[]{neiCost, neiIndex});
+            total_cost+=wt;
+            visited[nd]=1;
+            for(Pair p: adj.get(nd)){
+                int neighbor= p.node;
+                int edgWt=p.weight;
+                if(visited[neighbor]==0){
+                    pq.offer(new Pair(neighbor, edgWt));
                 }
             }
         }
-        return res;
+        return total_cost;
+        
     }
 }
