694f037f6487959312e50b08d3b12af7c4afbda2
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-10 11:06:32 -0400

    Time: 0 ms (100.00%) | Memory: 40.8 MB (90.67%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,37 +1,30 @@
 class Solution {
+    public int rob(int[] nums) {
+        int n= nums.length;
+        if(n==1) return nums[0];
 
-    private int accomplice(int[] nums,int index,int[] dp){
-        if(index< 0) return 0;
+        int[] sub_arr1=Arrays.copyOfRange(nums, 1, n);
 
+        int[] sub_arr2=Arrays.copyOfRange(nums, 0, n-1);
+
+        int[] dp1= new int[n-1];
+        int[] dp2= new int[n-1];
+        
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+        
+        return Math.max(dfs(sub_arr1, dp1,n-2), dfs(sub_arr2, dp2, n-2));
+    }
+
+    private int dfs(int[] nums, int[] dp, int index ){
+        if(index<0) return 0;
+        if(index==0) return nums[0];
         if(dp[index]!=-1){
             return dp[index];
         }
+        int take=  nums[index] + dfs(nums, dp, index-2);
+        int not_take= dfs(nums, dp, index-1);
 
-        int one= nums[index]+ accomplice(nums,index-2, dp);
-        int two= accomplice(nums,index-1,dp);
-
-        return dp[index]=Math.max(one ,two);
-
-    }
-
-
-    public int rob(int[] nums) {
-
-        int n=nums.length;
-        //edge case important
-        if(n==1) return nums[0];
-
-        int[] subarray1=Arrays.copyOfRange(nums,1,n);
-        int[] dp1=new int[subarray1.length];
-        Arrays.fill(dp1,-1);
-
-        int[] subarray2=Arrays.copyOfRange(nums,0,n-1);
-        int[] dp2=new int[subarray2.length];
-        Arrays.fill(dp2,-1);
-
-       
-       
-
-        return Math.max(accomplice(subarray1,subarray1.length-1,dp1),accomplice(subarray2,subarray2.length-1,dp2));  
+        return dp[index]= Math.max(take, not_take);
     }
 }
