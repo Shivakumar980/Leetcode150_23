75ed558f9b57bbebaef4eaccc7db0b625d2079e5
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-10 21:26:36 -0400

    Time: 27 ms (28.62%) | Memory: 45.6 MB (15.45%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,26 +1,35 @@
 class Solution {
-
     public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int[][] dp=new int[n][amount+1];
 
-        for(int amt=0; amt <= amount ;amt++){
-              dp[0][amt]= amt % coins[0]==0 ? amt/coins[0] : (int) 1e9 ;
-        }
-      
-       for(int ind=1; ind<n;ind++){
-            for(int target=0;target<=amount;target++){
-
-                int not_take= 0 + dp[ind-1][target];
-                int take= (int)1e9;
-                if(coins[ind]<=target){
-                    take= 1+ dp[ind][target-coins[ind]];
-                }
-                dp[ind][target]=Math.min(take, not_take);
+            int n= coins.length;
+            int[][] dp= new int[n][amount+1];
+            for(int[] oneD: dp){
+                 Arrays.fill(oneD,-1);
             }
-       }
+            int result= dfs(coins, n-1, amount, dp);
 
-       return dp[n-1][amount]==(int)1e9 ? -1 : dp[n-1][amount];
+            return result==(int)1e9 ? -1: result;
+    }
+
+    private int dfs(int[] coins, int ind, int target, int[][] dp){
+        if(ind==0){
+            if(target%coins[0]==0){
+                return target/coins[0];
+            }
+            else{
+                return (int)1e9;
+            }
+            }
+            if(dp[ind][target]!=-1){
+                return dp[ind][target];
+            }
+
+            int not_take=dfs(coins,ind-1, target,dp);
+            int take=  (int)1e9;
+            if(coins[ind]<=target){
+                take= 1+ dfs(coins, ind, target-coins[ind], dp);
+            }
+            return dp[ind][target]=Math.min(take,not_take);
         
     }
 }
