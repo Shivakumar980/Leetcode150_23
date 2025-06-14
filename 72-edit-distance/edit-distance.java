89bcedd8de1c25596ab454e8da9702df3e87c655
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-14 19:47:48 -0400

    Time: 3 ms (94.39%) | Memory: 45.6 MB (7.00%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,30 +1,32 @@
 class Solution {
-
-
-    public int minDistance(String s, String t) {
-        int n1 = s.length();
-        int n2 = t.length();
-
-        int[][] dp = new int[n1+1][n2+1];
-
-        for(int ind2=0; ind2<=n2; ind2++){
-            dp[0][ind2]=ind2;
+    public int minDistance(String word1, String word2) {
+        int n1= word1.length();
+        int n2= word2.length();
+        int[][] dp= new int[n1][n2];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-        for(int ind1=0; ind1<=n1 ;ind1++){
-            dp[ind1][0]=ind1;
-        }
+        return backtrack(n1-1, n2-1 , dp, word1, word2 );
+    }
 
-        for(int ind1=1;ind1<=n1;ind1++){
-            for(int ind2=1; ind2<=n2 ;ind2++){
-               if(s.charAt(ind1-1)==t.charAt(ind2-1)){
-                dp[ind1][ind2]=dp[ind1-1][ind2-1];
-               }
-               else{
-                dp[ind1][ind2]= Math.min(1+dp[ind1][ind2-1],Math.min(1+dp[ind1-1][ind2], 1+dp[ind1-1][ind2-1]));
-               }
-            }
+    private int backtrack(int ind1, int ind2, int[][] dp, String s, String t){
+        if(ind1<0){
+            return ind2+1;
+        }
+        if(ind2<0){
+            return ind1+1;
         }
 
-        return dp[n1][n2];
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
+        if(s.charAt(ind1)==t.charAt(ind2)){
+            dp[ind1][ind2]= backtrack(ind1-1, ind2-1, dp, s, t);
+        }
+        else{
+            dp[ind1][ind2]= Math.min(1+backtrack(ind1, ind2-1, dp, s, t) ,
+            Math.min(1+backtrack(ind1-1, ind2, dp, s, t), 1+backtrack(ind1-1, ind2-1, dp, s, t)));
+        }
+        return dp[ind1][ind2];
     }
 }
