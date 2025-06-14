0ec6dc84bd2a21f89edb9e13bfe9d4fb1e66320c
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-06-14 18:44:12 -0400

    Time: 18 ms (79.99%) | Memory: 51.8 MB (13.29%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,26 +1,32 @@
 class Solution {
-
     public int numDistinct(String s, String t) {
         int n1=s.length();
         int n2=t.length();
 
-        int[][] dp=new int[n1+1][n2+1];
-
-        for(int i=0;i<=n1;i++){
-            dp[i][0]=1;
+        int[][] dp= new int[n1][n2];
+        for(int[] row: dp){
+            Arrays.fill(row,-1);
         }
 
-        for(int ind1=1;ind1<=n1;ind1++){
-            for(int ind2=1; ind2<=n2 ;ind2++){
-                if(s.charAt(ind1-1)==t.charAt(ind2-1)){
-                    dp[ind1][ind2]= dp[ind1-1][ind2-1] +dp[ind1-1][ind2];
-                }
-                else{
-                    dp[ind1][ind2]= dp[ind1-1][ind2];
-                }
-            }
+        return backtrack(n1-1, n2-1, s, t, dp);
+    }
+    private int backtrack(int ind1, int ind2, String s, String t, int[][] dp){
+        if(ind2<0){
+            return 1;
         }
-        
-        return  dp[n1][n2];
+        if(ind1<0){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
+
+        if(s.charAt(ind1)==t.charAt(ind2)){
+             dp[ind1][ind2]= backtrack(ind1-1,ind2-1, s, t, dp) + backtrack(ind1-1,ind2, s, t, dp);
+        }
+        else{
+            dp[ind1][ind2]=backtrack(ind1-1,ind2, s, t, dp);
+        }
+        return dp[ind1][ind2];
     }
 }
