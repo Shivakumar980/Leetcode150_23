ead0546963d0aa4e4808954bdea14952152c7bb2
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-07-25 14:45:01 -0400

    Time: 2 ms (78.81%) | Memory: 44.8 MB (32.63%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,51 +1,56 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        int n=board.length;
+        int m=board[0].length;
 
-       
-
-        for(int i=0;i<9;i++){
-            HashSet<Character> rowset=new HashSet<>();
-            for(int j=0 ;j<9 ;j++){
-                if(rowset.contains(board[i][j])){
-                    return false;
-                }
-                if(board[i][j]!='.'){
-                     rowset.add(board[i][j]);
-                }
-               
-            }
-            HashSet<Character> colset=new HashSet<>();
-            for(int j=0 ;j<9 ;j++){
-                if(colset.contains(board[j][i])){
-                    return false;
-                }
-                 if(board[j][i]!='.'){
-                    colset.add(board[j][i]);
-                }
-                
-            }
-             
-        }
-        for(int box=0;box<9;box++){
-            int i=box/3;
-            int j=box%3;
-            HashSet<Character> dgset=new HashSet<>();
-            //System.out.println("hi");
-            for(int row=3*i;row<3*i+3;row++){
-                for(int col= 3*j;col<3*j+3; col++){
-                   if(dgset.contains(board[row][col])){
-                    return false;
-                   }
-                   if(board[row][col]!='.'){
-                          dgset.add(board[row][col]);
-                   }
-                 
-                }
-            }
-        }
-
-            return true;
-    }
         
-     
+        for(int i=0; i< n ;i++){
+            HashSet<Character> hs= new HashSet<>();
+            for(int j=0; j< m;j++){
+                if(board[i][j]=='.'){
+                    continue;
+                }
+                if(hs.contains(board[i][j])){
+                    return false;
+                }
+                hs.add(board[i][j]);
+            }
+        }
+         
+        for(int i=0; i< n ;i++){
+             HashSet<Character> hs1= new HashSet<>();
+            for(int j=0; j< m;j++){
+                if(board[j][i]=='.'){
+                    continue;
+                }
+                if(hs1.contains(board[j][i])){
+                    return false;
+                }
+                hs1.add(board[j][i]);
+            }
+        }
+ 
+        for(int box=0; box<9 ;box++){
+ HashSet<Character> hs2= new HashSet<>();
+                int row= (box/3)*3;
+                int col= (box%3)*3;
+
+                for(int i=row; i<3+row; i++){
+                    for(int j=col; j<3+col; j++){
+
+                         if(board[i][j]=='.'){
+                    continue;
+                }
+                if(hs2.contains(board[i][j])){
+                    return false;
+                }
+                hs2.add(board[i][j]);
+
+                    }
+                }
+
+
+        }
+        return true;
+    }
 }
