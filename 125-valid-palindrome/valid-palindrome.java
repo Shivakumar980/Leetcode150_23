cc677f457716b4f28fd15d982bcc3fe84ec6de9a
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-07-25 18:44:35 -0400

    Time: 3 ms (72.84%) | Memory: 43.7 MB (54.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,27 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
+        int n=s.length();
         StringBuilder sb= new StringBuilder();
-
         for(char c: s.toCharArray()){
             if(Character.isLetterOrDigit(c)){
                 sb.append(Character.toLowerCase(c));
             }
         }
-
-        String ss= sb.toString();
-
         int left=0;
-        int right=ss.length()-1;
-
+        int right=sb.length()-1;
+         s=sb.toString();
         while(left<=right){
-            if(ss.charAt(left)!=ss.charAt(right)){
+            if(s.charAt(left)!=s.charAt(right)){
                 return false;
             }
             left++;
             right--;
         }
         return true;
-        
     }
 }
