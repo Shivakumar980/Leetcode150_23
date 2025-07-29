5c2d4a7989ef346fca0381b8b337d614a68abbf6
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-07-29 15:45:15 -0400

    Time: 20 ms (50.32%) | Memory: 45.2 MB (46.28%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,33 +1,36 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int n=piles.length;
-        if(n==1) return (int)Math.ceil((double)piles[0]/h);
-        
-        int low = 1;
-        int high= Integer.MIN_VALUE;
-        for(int pile : piles){
-            high= Math.max(pile,high);
+
+    private int calculate_hours(int[] piles , int k){
+        double hours=0;
+        for(int num: piles){
+            hours+=Math.ceil((double)num/k);
         }
-
+        return  (int)hours;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int n= piles.length;
+        int ans=0;
+        int low=1;
+        int high=Integer.MIN_VALUE;
+        for(int num: piles){
+            high=Math.max(high, num);
+        }
+        
         while(low<=high){
-            int mid= low+(high-low)/2;
+            int mid= low+ (high-low)/2;
+            
+            int hours= calculate_hours(piles, mid);
 
-            double timetaken=0;
-            for(int i=0; i< n;i++){
-                timetaken+= (int)Math.ceil((double)piles[i]/mid);
+            if(hours> h){
+                low = mid+1;
             }
-
-            if(timetaken> h){
-                low=mid+1;
-            }
-            else if(timetaken<=h){
+            else{
+                ans=mid;
                 high=mid-1;
             }
 
-            
-     }
-      return low;
-    }
+        }
+        return ans;
 
-    
+    }
 }
