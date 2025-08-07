1f1833fa1daca87da846b77d523fde61c96fb4bb
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-08-06 20:40:39 -0400

    Time: 4 ms (91.64%) | Memory: 46.3 MB (28.07%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,33 +1,35 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         int n= intervals.length;
-
-        int[] start= new int[n];
+        int[] start=new int[n];
         int[] end= new int[n];
-        for(int i=0 ; i< n ;i++){
-            start[i]=intervals[i][0];
+
+        for(int i=0; i< n ;i++){
+            start[i]= intervals[i][0];
             end[i]= intervals[i][1];
         }
 
         Arrays.sort(start);
         Arrays.sort(end);
 
-        int s=0; 
+        int s=0;
         int e=0;
-        int rooms=0;
-        int res=0;
+
+        int count=0;
+        int ans=0;
         while(s<n){
             if(start[s]<end[e]){
-                rooms++;
+                count+=1;
                 s++;
-            }   
+            }
             else{
-                rooms--;
+                count-=1;
                 e++;
             }
-            res=Math.max(res, rooms);
+             ans= Math.max(ans, count);
         }
+       
+    return ans;
 
-        return res;
     }
 }
