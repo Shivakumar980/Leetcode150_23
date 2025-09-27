1bf9f0040514d5ffdf2b569ca60899a8050c0b23
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-09-27 18:32:55 -0400

    Time: 9 ms (37.99%) | Memory: 43 MB (7.47%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -3,21 +3,23 @@ class Solution {
         int n= s.length();
         List<Integer> result= new ArrayList<>();
         HashMap<Character, Integer> hm= new HashMap<>();
-        for( int i=0; i< n; i++){
-            hm.put(s.charAt(i),i);
+        for(int i=0; i< n ;i++){
+            char ch=s.charAt(i);
+            hm.put(ch,i);
         }
 
         int end=0;
         int size=0;
-        for(int i=0; i< n ;i++){
-            char ch= s.charAt(i);
+        for(int i=0; i<n ;i++){
             size+=1;
-            end=Math.max(end, hm.get(ch));
+            end= Math.max(hm.get(s.charAt(i)),end);
             if(end==i){
                 result.add(size);
                 size=0;
             }
+
         }
         return result;
+
     }
 }
