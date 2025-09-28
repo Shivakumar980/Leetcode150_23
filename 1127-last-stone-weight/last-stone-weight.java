78f3c302249ef118dd4a102248283bbf8e38d269
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-09-28 09:31:40 -0400

    Time: 1 ms (98.78%) | Memory: 41.3 MB (33.41%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,26 +1,20 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq= new PriorityQueue<>((a,b)->b-a);
 
-        PriorityQueue<Integer> pq= new PriorityQueue<>((a,b)-> b-a);
-
-        for(int stone : stones){
-            pq.offer(stone);
+        for(int stone: stones ){
+            pq.add(stone);
         }
 
-       
-        while(pq.size()!=1){
-            int first= pq.poll();
-            int second= pq.poll();
-            if(first==second){
-                pq.offer(0);
-            }
-            else{
-                pq.offer(Math.abs(first-second));
-            }
-           
-        }
-        return pq.peek();
+        while(pq.size()>1){
+            int first=pq.poll();
+            int second=pq.poll();
 
-        
+            if(first!=second){
+                pq.add(Math.abs(first-second));
+            }
+        }
+
+        return pq.size()==1? pq.peek() :0 ;
     }
 }
