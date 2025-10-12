83ae7129fdbf850b491ba2fc9560e9f9cd792255
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-12 17:54:55 -0400

    Time: 2 ms (61.07%) | Memory: 44.4 MB (65.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int n=board.length;
+        int n= board.length;
         int m=board[0].length;
 
-        
-        for(int i=0; i< n ;i++){
+        for( int i=0 ; i< n ; i++){
             HashSet<Character> hs= new HashSet<>();
-            for(int j=0; j< m;j++){
+            for(int j=0; j< m ; j++){
                 if(board[i][j]=='.'){
                     continue;
                 }
@@ -16,41 +15,38 @@ class Solution {
                 hs.add(board[i][j]);
             }
         }
-         
-        for(int i=0; i< n ;i++){
-             HashSet<Character> hs1= new HashSet<>();
-            for(int j=0; j< m;j++){
-                if(board[j][i]=='.'){
+
+        for( int j=0 ; j< m ; j++){
+            HashSet<Character> hs= new HashSet<>();
+            for(int i=0; i< n ; i++){
+                 if(board[i][j]=='.'){
                     continue;
                 }
-                if(hs1.contains(board[j][i])){
+                if(hs.contains(board[i][j])){
                     return false;
                 }
-                hs1.add(board[j][i]);
+                hs.add(board[i][j]);
             }
         }
- 
-        for(int box=0; box<9 ;box++){
- HashSet<Character> hs2= new HashSet<>();
-                int row= (box/3)*3;
-                int col= (box%3)*3;
 
-                for(int i=row; i<3+row; i++){
-                    for(int j=col; j<3+col; j++){
+        for(int box=0 ; box < 9 ; box++){
+             HashSet<Character> hs= new HashSet<>();
+            int row= 3*(box/3);
+            int col= 3* (box%3);
+            for( int i= row ; i < row+ 3 ; i++){
+                 for( int j= col ; j <  col + 3 ; j++){
+                     if(board[i][j]=='.'){
+                        continue;
+                        }
 
-                         if(board[i][j]=='.'){
-                    continue;
-                }
-                if(hs2.contains(board[i][j])){
+                     if(hs.contains(board[i][j])){
                     return false;
-                }
-                hs2.add(board[i][j]);
-
                     }
-                }
-
-
+                    hs.add(board[i][j]);
+                 }
+            }
         }
         return true;
     }
+
 }
