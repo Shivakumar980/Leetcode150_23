a5ed8697e212b522764461d3a632714e2f31f6e3
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-26 14:40:11 -0400

    Time: 3 ms (31.17%) | Memory: 45.3 MB (9.27%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,37 +1,40 @@
 class Solution {
-    int[] delrow={0,-1,0,1};
-    int[] delcol={-1,0,1,0};
+    int[] delrow={1,0,-1,0};
+    int[] delcol={0,1,0,-1};
     public int maxAreaOfIsland(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int count=0;
-        int[][] visited=new int[n][m];
-        
-        int maxarea=0;
+
+        int[][] visited= new int[n][m];
+
+        int maxArea=0;
+       
         for(int i=0; i< n; i++){
-            for(int j=0;j<m ;j++ ){
-                if(grid[i][j]==1 && visited[i][j]==0){
-                    int[] area={0};
-                    area=dfs(i, j, grid, visited,area);
-                    maxarea= Math.max(maxarea,area[0]);
+            for(int j=0; j<m ;j++){
+                int[] area={1};
+                if(visited[i][j]==0 && grid[i][j]==1){
+                    area=dfs(i, j, visited, grid, area);
+                    maxArea=Math.max(area[0], maxArea);
                 }
             }
         }
-        return maxarea;
+
+        return maxArea;
+
     }
 
-    private int[] dfs(int row, int col, int[][] grid, int[][] visited,int[] area){
-
-        if(row<0 || row==grid.length || col<0 || col==grid[0].length || grid[row][col]==0 || visited[row][col]==1){
-            return new int[]{0};
-        }
+    private int[] dfs(int row, int col, int[][] visited, int[][] grid, int[] area){
         visited[row][col]=1;
-        area[0]+=1;
+
         for(int i=0; i< 4; i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            dfs(nrow,ncol, grid,visited,area);
+            int nrow= row+delrow[i];
+            int ncol= col+delcol[i];
+            if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol< grid[0].length && grid[nrow][ncol]==1 && visited[nrow][ncol]==0){
+            area[0]+=1;
+            dfs(nrow, ncol, visited, grid, area);
         }
+        }
+        
         return area;
     }
 }
