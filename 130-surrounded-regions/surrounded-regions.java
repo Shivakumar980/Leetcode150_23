1d6885e728d196a92274f54e8d4e50de76b70b6c
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-26 16:59:01 -0400

    Time: 2 ms (83.18%) | Memory: 46.1 MB (7.64%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,39 +1,39 @@
 class Solution {
-    int[] delrow= {0,-1,0,1};
-    int[] delcol= {-1,0,1,0};
+    int[] delrow= {1,0,-1,0};
+    int[] delcol= {0,1,0,-1};
     public void solve(char[][] board) {
         int n= board.length;
         int m= board[0].length;
-        int[][] visited= new int[n][m];
-
-        for(int i=0 ; i< n ;i++){
-            for(int j=0 ; j< m ; j++){
-                if(visited[i][j]==0 && (i==0 || i==n-1||j==0 || j==m-1) && board[i][j]=='O'){
-                   dfs(i, j, board, visited);
-                }
-            }
+        int[][] visited=new int[n][m];
+        for(int i=0; i< n ; i++){
+            for(int j=0 ; j<m ;j++){
+                if((i==0 || i==n-1 || j==0 || j==m-1) && visited[i][j]==0 && board[i][j]=='O'){
+                    dfs(i, j, board, visited);
+                }               
+           }
+        }
+        for(int i=0; i< n ; i++){
+            for(int j=0 ; j<m ;j++){
+                if( visited[i][j]==0){
+                   board[i][j]='X';
+                }               
+           }
         }
 
-        for(int i=0 ; i< n ;i++){
-            for(int j=0 ; j< m ; j++){
-                if(visited[i][j]==0 && i!=0 && i!=n-1 && j!=0 && j!=m-1){
-                    board[i][j]='X';
-                }
-            }
-        }
+
     }
-
     private void dfs(int row, int col, char[][] board, int[][] visited){
-        if(row<0 || col<0 || row==board.length || col==board[0].length || visited[row][col]==1 || board[row][col]=='X'){
-            return;
-        }
         visited[row][col]=1;
-        for(int i=0 ; i< 4; i++){
-            int nrow= row+delrow[i];
-            int ncol= col+delcol[i];
+        int n= board.length;
+        int m= board[0].length;
+        for(int i=0; i<4 ;i++){
+            int nrow= row+ delrow[i];
+            int ncol= col+ delcol[i];
 
-            dfs(nrow, ncol, board, visited);
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0 && board[nrow][ncol]=='O'){
+                dfs(nrow, ncol, board, visited);
+            }
         }
-        return;
+
     }
 }
