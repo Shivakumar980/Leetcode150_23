fca48354f5a69e918a45e8fedc5eb0d402c1469e
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-26 14:52:07 -0400

    Time: 2 ms (68.48%) | Memory: 44.7 MB (52.28%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -11,10 +11,10 @@ class Solution {
        
         for(int i=0; i< n; i++){
             for(int j=0; j<m ;j++){
-                int[] area={1};
+                
                 if(visited[i][j]==0 && grid[i][j]==1){
-                    area=dfs(i, j, visited, grid, area);
-                    maxArea=Math.max(area[0], maxArea);
+                    int area=dfs(i, j, visited, grid);
+                    maxArea=Math.max(area, maxArea);
                 }
             }
         }
@@ -23,15 +23,14 @@ class Solution {
 
     }
 
-    private int[] dfs(int row, int col, int[][] visited, int[][] grid, int[] area){
+    private int dfs(int row, int col, int[][] visited, int[][] grid){
         visited[row][col]=1;
-
+        int area=1;
         for(int i=0; i< 4; i++){
             int nrow= row+delrow[i];
             int ncol= col+delcol[i];
             if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol< grid[0].length && grid[nrow][ncol]==1 && visited[nrow][ncol]==0){
-            area[0]+=1;
-            dfs(nrow, ncol, visited, grid, area);
+            area+=dfs(nrow, ncol, visited, grid);
         }
         }
         
