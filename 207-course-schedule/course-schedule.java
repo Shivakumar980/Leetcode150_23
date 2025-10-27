1b54976a9f17f2534aae30cc60f98b8f363f7c63
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-26 21:21:58 -0400

    Time: 7 ms (44.35%) | Memory: 45.5 MB (54.96%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -2,44 +2,38 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
         List<List<Integer>> adj= new ArrayList<>();
-
-        for(int i=0; i< numCourses; i++){
+        for(int i=0 ; i<numCourses ;i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int[] edge: prerequisites){
-            adj.get(edge[1]).add(edge[0]);
-        }
-
         int[] indegree= new int[numCourses];
-
-        for(int[] edge: prerequisites){
+        for(int[] edge:prerequisites){
+            adj.get(edge[1]).add(edge[0]);
             indegree[edge[0]]++;
         }
 
         Queue<Integer> q= new LinkedList<>();
-        for(int i=0 ; i< numCourses; i++){
+        for(int i=0 ; i<numCourses; i++){
             if(indegree[i]==0){
                 q.offer(i);
             }
         }
-        int[] ans= new int[numCourses];
-        int ind=0;
+
+        int size=0;
         while(!q.isEmpty()){
-            int node= q.poll();
-            ind++;
-            for(int neighbour: adj.get(node)){
-                indegree[neighbour]--;
-                if(indegree[neighbour]==0){
-                    q.offer(neighbour);
+            int node= q.peek();
+            size+=1;
+            q.poll();
+            for(int neighbor: adj.get(node)){
+                indegree[neighbor]--;
+                if(indegree[neighbor]==0){
+                    q.offer(neighbor);
                 }
             }
         }
-
-        return numCourses==ind;
-
        
-
+        return size==numCourses;
         
+
     }
 }
