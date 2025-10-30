d34ee7a65bb151875a3ab9913a090dc49455386d
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-30 18:06:40 -0400

    Time: 9 ms (93.35%) | Memory: 48.9 MB (56.28%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,50 +1,51 @@
 class Pair{
     int node;
-    int distance;
-    public Pair(int node, int distance){
-        this.node= node;
-        this.distance= distance;
+    int weight;
+
+    public Pair(int node, int weight){
+        this.node=node;
+        this.weight=weight;
     }
 }
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-
         List<List<Pair>> adj= new ArrayList<>();
-        for(int i=0 ; i<= n ;i++){
-            adj.add(new ArrayList<Pair>());
+        for(int i=0; i<= n ;i++){
+            adj.add(new ArrayList<>());
         }
 
-        for(int[] time : times){
-            
+        for(int[] time: times){
             adj.get(time[0]).add(new Pair(time[1],time[2]));
         }
 
-        int[] dist= new int[n+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[k]=0;
-        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)-> a.distance-b.distance);
+        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)->a.weight-b.weight);
         pq.add(new Pair(k,0));
-
+        int[] dist= new int[n+1];
+        Arrays.fill(dist, (int)1e9);
+        dist[k]=0;
         while(!pq.isEmpty()){
-            Pair p=pq.poll();
-            int node= p.node;
-            int distance=p.distance;
+            int node= pq.peek().node;
+            int time= pq.peek().weight;
+            pq.poll();
 
-            for(Pair pr: adj.get(node)){
-                int nd= pr.node;
-                int d= pr.distance;
-                if(distance+d < dist[nd]){
-                    dist[nd]= distance+d;
-                    pq.offer(new Pair(nd, dist[nd]));
+            for(Pair p: adj.get(node)){
+                int nd=p.node;
+                int wt=p.weight;
+
+                if( wt + time< dist[nd]){
+                    dist[nd]= wt+time;
+                    pq.add(new Pair(nd, dist[nd]));
                 }
             }
+
         }
-        int mini=0;
-        for(int i=1; i<= n ;i++){
-            if(dist[i]==Integer.MAX_VALUE) return -1;
-            mini= Math.max(mini, dist[i]);
+        int res= Integer.MIN_VALUE;
+        for(int i=1; i<=n ;i++){
+            if(dist[i]==(int)1e9) return -1;
+            else{
+                res= Math.max(res, dist[i]);
+            }
         }
-        return mini;
-        
+        return res;
     }
 }
