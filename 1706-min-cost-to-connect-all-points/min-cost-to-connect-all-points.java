5e36f6c8111bdd9283361d0e40449c1a11fc62e4
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-31 13:54:14 -0400

    Time: 674 ms (12.62%) | Memory: 135 MB (5.07%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,54 +1,63 @@
 class Pair{
     int node;
     int weight;
+
     public Pair(int node, int weight){
-        this.node=node;
-        this.weight=weight;
+        this.node= node;
+        this.weight= weight;
     }
 }
-
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-
+        
+        int n=points.length;
         List<List<Pair>> adj= new ArrayList<>();
-        int n= points.length;
-
-        for(int i=0 ;i< n ;i++){
-            adj.add(new ArrayList<Pair>());
+        for(int i=0; i<n ; i++){
+            adj.add(new ArrayList<>());
         }
+        
+        for(int i=0; i<n ; i++){
+            int x1=points[i][0];
+            int y1=points[i][1];
 
-        for(int i=0; i< n ;i++){
-            for(int j=i+1; j< n ;j++){
-                int distance= Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
-                adj.get(i).add(new Pair(j,distance));
-                adj.get(j).add(new Pair(i,distance));
+            for(int j=i+1; j<n ; j++){
+                int x2= points[j][0];
+                int y2= points[j][1];
+                
+                int dist= Math.abs(x1-x2) + Math.abs(y1-y2);
+                adj.get(i).add(new Pair(j,dist));
+                adj.get(j).add(new Pair(i,dist));
             }
+
         }
 
-        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b)-> a.weight-b.weight);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.weight-b.weight);
         int[] visited= new int[n];
-        pq.add(new Pair(0,0));
-       
 
-        int total_cost=0;
+        pq.offer(new Pair(0,0));
+
+        int res=0;
         while(!pq.isEmpty()){
             int nd= pq.peek().node;
             int wt= pq.peek().weight;
             pq.poll();
-            if(visited[nd]==1){
-                continue;
-            }
-            total_cost+=wt;
+            if(visited[nd]==1) continue;
             visited[nd]=1;
+            res+=wt;
             for(Pair p: adj.get(nd)){
-                int neighbor= p.node;
-                int edgWt=p.weight;
-                if(visited[neighbor]==0){
-                    pq.offer(new Pair(neighbor, edgWt));
+                int nnd=p.node;
+                int wwt=p.weight;
+                if(visited[nnd]==0){
+                    pq.offer(p);
                 }
+                
             }
+
         }
-        return total_cost;
+
+        return res;
+        
+
         
     }
 }
