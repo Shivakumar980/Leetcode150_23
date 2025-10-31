366c630d528d44c35a7f53bfef5567bf88bfda91
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-10-31 15:48:26 -0400

    Time: 377 ms (38.93%) | Memory: 98.2 MB (8.07%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -7,15 +7,55 @@ class Pair{
         this.weight= weight;
     }
 }
+
+class Disjoint{
+    int[] rank;
+    int[] parent;
+
+    public Disjoint(int n){
+        rank= new int[n];
+        parent= new int[n];
+
+        for(int i=0; i< n ;i++){
+            rank[i]=1;
+            parent[i]=i;
+        }
+    }
+
+        public int findParent(int x){
+            if(x!=parent[x]){
+                parent[x]=findParent(parent[x]);
+                return parent[x];
+            }
+            else{
+                return x;
+            }
+        }
+
+        public boolean union(int u, int v){
+            int ulp_u= findParent(u);
+            int ulp_v= findParent(v);
+
+            if(ulp_u==ulp_v) return false;
+            
+            if(rank[ulp_u]<rank[ulp_v]){
+                parent[ulp_u]=ulp_v;
+            }
+            else if(rank[ulp_v]< rank[ulp_u]){
+                parent[ulp_v]=ulp_u;
+            }
+            else{
+                parent[ulp_u]=ulp_v;
+                rank[ulp_v]++;
+            }
+            return true;
+        }
+}
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         
         int n=points.length;
-        List<List<Pair>> adj= new ArrayList<>();
-        for(int i=0; i<n ; i++){
-            adj.add(new ArrayList<>());
-        }
-        
+        List<int[] > adj= new ArrayList<>();
         for(int i=0; i<n ; i++){
             int x1=points[i][0];
             int y1=points[i][1];
@@ -25,39 +65,22 @@ class Solution {
                 int y2= points[j][1];
                 
                 int dist= Math.abs(x1-x2) + Math.abs(y1-y2);
-                adj.get(i).add(new Pair(j,dist));
-                adj.get(j).add(new Pair(i,dist));
+                adj.add(new int[]{i,j, dist});
+                //adj.get(j).add(new Pair(i,dist));
             }
-
         }
-
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.weight-b.weight);
-        int[] visited= new int[n];
-
-        pq.offer(new Pair(0,0));
-
+        Collections.sort(adj, (a,b)->a[2]-b[2]);
+        Disjoint dj= new Disjoint(n);
         int res=0;
-        while(!pq.isEmpty()){
-            int nd= pq.peek().node;
-            int wt= pq.peek().weight;
-            pq.poll();
-            if(visited[nd]==1) continue;
-            visited[nd]=1;
-            res+=wt;
-            for(Pair p: adj.get(nd)){
-                int nnd=p.node;
-                int wwt=p.weight;
-                if(visited[nnd]==0){
-                    pq.offer(p);
-                }
-                
+        for(int[] tuple:adj){
+            if(dj.union(tuple[0],tuple[1])){
+                res+=tuple[2];
             }
-
         }
 
         return res;
-        
 
         
     }
+
 }
