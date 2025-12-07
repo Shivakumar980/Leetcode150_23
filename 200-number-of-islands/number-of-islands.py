d70da5045700e16fd3dabdcba802d2e021469e51
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-12-07 16:03:39 -0500

    Time: 243 ms (63.42%) | Memory: 20.3 MB (56.80%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,31 +1,36 @@
 class Solution:
-    delrow=[0,1,-1,0]
-    delcol=[-1,0,0,1]
     def numIslands(self, grid: List[List[str]]) -> int:
-        n=len(grid)
-        m=len(grid[0])
+        n= len(grid)
+        m= len(grid[0])
 
-        visited=[[0]*m for _ in range(n)]
+        visited= [[0]*m for _ in range(n)]
 
-        def dfs( row, col, visited, grid):
+        def bfs(visited, grid, row, col):
+            n= len(grid)
+            m= len(grid[0])
             visited[row][col]=1
-            
-            for i in range(4):
-                nrow= row+ self.delrow[i]
-                ncol= col +self.delcol[i]
+            q= deque()
+            q.append((row, col))
+            directions= [(1,0),(0,1),(-1,0),(0,-1)]
 
-                if(nrow>=0 and nrow < len(grid) and ncol>=0  and ncol <len(grid[0]) and visited[nrow][ncol]==0 and grid[nrow][ncol]=="1"):
-                    dfs(nrow, ncol, visited, grid)
+            while q:
+                nr, nc = q.popleft()
+                visited[row][col]=1
 
-                    
+                for r,c in directions:
+                    nrow= r+nr
+                    ncol= c+nc
+
+                    if(0<=nrow< n and 0<= ncol <m and grid[nrow][ncol]=="1" and visited[nrow][ncol]==0):
+                        visited[nrow][ncol]=1
+                        q.append((nrow,ncol))
+
 
         res=0
         for i in range(n):
             for j in range(m):
-                if visited[i][j]==0 and grid[i][j]=="1" :
-                    dfs(i, j, visited, grid)
+                if visited[i][j]==0 and grid[i][j]=="1":
+                    bfs(visited, grid, i, j )
                     res+=1
         return res
-    
-    
-    
+        
