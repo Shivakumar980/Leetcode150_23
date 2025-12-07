3915dc786c9778a60724b27554271e8107dadfb1
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2025-12-07 15:05:58 -0500

    Time: 263 ms (35.11%) | Memory: 19.9 MB (95.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -7,21 +7,25 @@ class Solution:
 
         visited=[[0]*m for _ in range(n)]
 
+        def dfs( row, col, visited, grid):
+            visited[row][col]=1
+            
+            for i in range(4):
+                nrow= row+ self.delrow[i]
+                ncol= col +self.delcol[i]
+
+                if(nrow>=0 and nrow < len(grid) and ncol>=0  and ncol <len(grid[0]) and visited[nrow][ncol]==0 and grid[nrow][ncol]=="1"):
+                    dfs(nrow, ncol, visited, grid)
+
+                    
+
         res=0
         for i in range(n):
             for j in range(m):
                 if visited[i][j]==0 and grid[i][j]=="1" :
-                    self.dfs(i, j, visited, grid)
+                    dfs(i, j, visited, grid)
                     res+=1
         return res
     
-    def dfs(self, row, col, visited, grid):
-        visited[row][col]=1
-        
-        for i in range(4):
-            nrow= row+ self.delrow[i]
-            ncol= col +self.delcol[i]
-
-            if(nrow>=0 and nrow < len(grid) and ncol>=0  and ncol <len(grid[0]) and visited[nrow][ncol]==0 and grid[nrow][ncol]=="1"):
-                self.dfs(nrow, ncol, visited, grid)
+    
     
