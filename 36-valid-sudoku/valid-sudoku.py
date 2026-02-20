6df031150b3ffd593ff0ffd7525098fd8a4d3bdf
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2026-02-20 01:17:47 -0500

    Time: 4 ms (51.67%) | Memory: 19.3 MB (57.64%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,38 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        n= len(board)
-        m= len(board[0])
+        rowmap=defaultdict(set)
+        colmap=defaultdict(set)
+        boxmap=defaultdict(set)
 
-        for i in range(n):
-            hs=set()
-            for j in range(m):
-                if board[i][j]=='.':
+        for row in range(9):
+            for col in range(9):
+                if board[row][col]=='.':
                     continue
-                if board[i][j] in hs:
-                    return False
-                hs.add(board[i][j])
-        
-        for i in range(m):
-            hs=set()
-            for j in range(n):
-                if board[j][i]=='.':
-                    continue
-                if board[j][i] in hs:
-                    return False
-                hs.add(board[j][i])
-        
-        for box in range(9):
-            hs=set()
-            for i in range(3):
-                for j in range(3):
-                    row= (box//3 )*3+i
-                    col= (box%3)*3+j
-                    if board[row][col]=='.':
-                        continue
-                    if board[row][col] in hs:
+                else:
+                    if(board[row][col] in rowmap[row] or board[row][col] in colmap[col] or board[row][col] in boxmap[(row//3,col//3)]):
                         return False
-                    hs.add(board[row][col])
+                    rowmap[row].add(board[row][col])
+                    colmap[col].add(board[row][col])
+                    boxmap[(row//3,col//3)].add(board[row][col])
         return True
-
-
