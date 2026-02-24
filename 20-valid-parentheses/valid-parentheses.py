bb6d86098a6ea24912234ccf15af4dfb038e736f
Shivakumar Machidi <63966962+Shivakumar980@users.noreply.github.com>
2026-02-24 17:41:22 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.45%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -9,9 +9,9 @@ class Solution:
                 st.append('}')
             elif c=='[':
                 st.append(']')
-            elif len(st)==0 or st[-1]!=c:
+            elif not st or st[-1]!=c:
                 return False
             else:
                 st.pop()
             
-        return len(st)==0
+        return not st
